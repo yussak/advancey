824310a6ca21aa58b6467620da136074f95997fe
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-09-06 15:40:57 +0900

    Add full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
-end
+
+    def full_title(page_title = '')
+      base_title = "Bookworm"
+      if page_title.empty?
+        base_title
+      else
+        page_title + " / " + base_title
+      end
+    end
+  end
