f4cab485de9423de4058ea4be54b36de7071a645
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-09-08 05:01:27 +0900

    enable Bootstrap 5

---
+++ app/javascript/js/hoge.js
@@ -0,0 +1,13 @@
+// bootstrap のツールボール＆ポップオーバー初期化処理
+window.bootstrap = require("bootstrap");
+document.addEventListener('turbolinks:load', ()=> {
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+  })
+});
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,8 +1,3 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -11,3 +6,12 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+// 作成したファイルをwebpackerに読み込ませます。
+import "../stylesheets/application"
+import "../js/hoge.js"
+
+// - 検証用 -
+// app/javascript/images/フォルダー内の001.jpeg画像を
+// webpackerに読み込ませます。
+const images = require.context('../images', true)
