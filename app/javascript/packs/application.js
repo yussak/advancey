fbb5e038bca7a3446c1e4f7ea86de90335047342
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-09-08 20:41:12 +0900

    create minimum layout for TOP

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,7 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-// 作成したファイルをwebpackerに読み込ませます。
 import "../stylesheets/application"
 import "../js/hoge.js"
 
-// - 検証用 -
-// app/javascript/images/フォルダー内の001.jpeg画像を
-// webpackerに読み込ませます。
 const images = require.context('../images', true)
