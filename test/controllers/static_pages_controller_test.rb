1e302cc0b4de01a5f702e3dd4870e8c6e223688b
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-09-09 06:57:26 +0900

    create root_path,about_path,contact_path

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,25 +6,25 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Bookworm"
   end
 
-  test "should get root" do
-    get root_url
-    assert_response :success
-  end
-
-  # test "should get home" do
-  #   get static_pages_home_url
+  # test "should get root" do
+  #   get root_path
   #   assert_response :success
-  #   assert_select "title", "ホーム / #{@base_title}"
   # end
 
+  test "should get home" do
+    get root_path
+    assert_response :success
+    assert_select "title", "ホーム / #{@base_title}"
+  end
+
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "アプリ詳細 / #{@base_title}"
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
     assert_select "title", "お問い合わせ / #{@base_title}"
   end
