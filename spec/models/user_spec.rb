cc0937321ad13856f80840e465e49e31598691f8
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-09-12 00:04:15 +0900

    feat:nameとemailの文字数制限設定とテスト追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,4 +16,14 @@ RSpec.describe User, type: :model do
     user.email = " "
     expect(user).not_to be_valid
   end
+
+  it "has name that isn't too long" do
+    user.name = "a" * 51
+    expect(user).not_to be_valid
+  end
+
+  it "has email that isn't too long" do
+    user.email = "a" * 244 + "@example.com"
+    expect(user).not_to be_valid
+  end
 end
