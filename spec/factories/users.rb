2f7d11a884b64d5ecd8190acc5c164552622c0db
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-09-12 10:14:41 +0900

    fix:factory_botを導入してテスト改善した

---
+++ spec/factories/users.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+    factory :user do
+        name { "Example User" }
+        email { "user@example.com" }
+    end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,34 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:user){ build :user}
+
+    it "is valid" do
+      expect(user).to be_valid
+    end
   
-  user = User.new(name: "Example User", email: "user@example.com")
-  it "is valid" do
-    expect(user).to be_valid
-  end
-
-  it "is invalid without name" do
-    user.name = " "
-    expect(user).not_to be_valid
-  end
-
-  it "is invalid without email" do
-    user.email = " "
-    expect(user).not_to be_valid
-  end
+    it "is invalid without name" do
+      user.name = " "
+      expect(user).to be_invalid
+    end
+  
+    it "is invalid without email" do
+      user.email = " "
+      expect(user).to be_invalid
+    end
 
   it "has name that isn't too long" do
     user.name = "a" * 51
-    expect(user).not_to be_valid
+    expect(user).to be_invalid
   end
 
   it "has email that isn't too long" do
     user.email = "a" * 244 + "@example.com"
-    expect(user).not_to be_valid
+    expect(user).to be_invalid
   end
 
   it "email validation should accept valid addresses" do
-    user.name="tarou"
     valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
                          first.last@foo.jp alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
