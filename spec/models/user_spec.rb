d20205c2fd7991465340b4896056b071a1b2f570
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-09-12 12:28:34 +0900

    データベースに保存する前にemailを小文字に変換する

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -47,8 +47,14 @@ RSpec.describe User, type: :model do
 
   it "email addresses should be unique" do
     duplicate_user = user.dup
-    duplicate_user.email = user.email.upcase
     user.save
     expect(duplicate_user).to be_invalid
+  end
+
+  it "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    user.email = mixed_case_email
+    user.save
+    assert_equal mixed_case_email.downcase, user.reload.email
   end
 end
