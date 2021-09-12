d2ddaac2f7163dd5ba8b2b85ba584d746003c363
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-09-12 12:18:02 +0900

    feat:データベースレベルでメールアドレスの一意性を強制

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_11_093030) do
+ActiveRecord::Schema.define(version: 2021_09_12_024742) do
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
