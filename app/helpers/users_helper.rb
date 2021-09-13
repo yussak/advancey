65fc1498cadf9b44bc5b1dff605ea8500816d620
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-09-13 20:32:22 +0900

    fix:gravatarエラー修正

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,6 @@
 module UsersHelper
 
-    def gravatar_for(size: 80)
-      size = options[:size]
+    def gravatar_for(user,size: 80)
       gravatar_id  = Digest::MD5::hexdigest(user.email.downcase)
       gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
       image_tag(gravatar_url, alt: user.name, class: "gravatar")
