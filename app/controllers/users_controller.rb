09118f46984217c01ff09b687cca3a83a31babf6
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-09-16 05:36:41 +0900

    feat:新規登録時のメッセージ表示を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,8 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+    flash[:success] = "Bookwormへようこそ！"
     redirect_to @user
-
     else
       render 'new'
     end
