9b443f1dcab756022f3ab3f670a1917cbf7930cf
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-09-18 12:10:23 +0900

    test:登録成功時メッセージのテスト追加

--- spec/systems/users_spec.rb
+++ spec/systems/users_spec.rb
@@ -2,10 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'users', type: :system do
   describe 'user create a new account' do
-    context 'enter an invalid values' do
+    context '登録失敗時のエラーメッセージ' do
       before do
         visit signup_path
-        # fill_in 'name', with: ''
         fill_in 'user[name]', with: ''
         fill_in 'user[email]', with: ''
         fill_in 'user[password]', with: ''
@@ -13,8 +12,7 @@ RSpec.describe 'users', type: :system do
         click_button '新規登録'
       end
       subject { page }
-      #エラーの検証
-      it 'get errors' do
+      it '登録失敗時のメッセージ' do
         is_expected.to have_selector('#error_explanation')
         is_expected.to have_selector('.alert-danger', text: 'The form contains 6 errors.')
         is_expected.to have_content("Password can't be blank", count: 2)
@@ -23,6 +21,20 @@ RSpec.describe 'users', type: :system do
       it 'render to /signup url' do
         is_expected.to have_current_path '/users'
       end
+    end
+
+    context '登録成功時のメッセージ' do
+        before do
+            visit signup_path
+            fill_in 'user[name]', with: 'user'
+            fill_in 'user[email]', with: 'user@example.com'
+            fill_in 'user[password]', with: 'password'
+            fill_in 'user[password_confirmation]', with: 'password'
+            click_button '新規登録'
+        end
+        it '成功時メッセージが出る' do
+            expect(page).to have_selector('.alert-success', text: 'Bookwormへようこそ！')
+        end
     end
   end
 end
