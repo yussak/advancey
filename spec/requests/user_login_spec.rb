3861ac5d5c353f0853e133d8e32cee03cfad4f41
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-09-19 18:02:29 +0900

    test:emailが正しくてpasswordが誤っている場合のテスト追加

--- spec/requests/user_login_spec.rb
+++ spec/requests/user_login_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "User login", type: :request do
+
     before do
         get login_path
         expect(response).to have_http_status(200)
     end
+
     context 'ログイン失敗時' do
         it 'ログインに失敗してflash messageが出る' do
             post login_path, params: { session: { email: "", password: "" } }
@@ -21,11 +23,19 @@ RSpec.describe "User login", type: :request do
         before do
             @user = FactoryBot.create(:user)
         end
-        it 'a' do
+        it 'login with valid information' do
             post login_path, params: { session: { email: @user.email, password: @user.password} }
             expect(response).to redirect_to @user
             follow_redirect!
             expect(response).to render_template('users/show')
         end
+
+        it 'login with valid email & invalid password' do
+            post login_path, params: { session: { email: @user.email, password: 'invalid'} }
+            expect(flash[:danger]).to be_truthy
+            get root_path
+            expect(flash[:danger]).to be_falsey
+        end
     end
+
 end
