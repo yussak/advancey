a91f97bf70bab943a2f7a8d20dc8cb79ebd64b6c
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-09-25 20:57:58 +0900

    test:postのバリデーション追加

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:user)
+    @post = Post.new(content: 'Lorem ipsum', user_id: @user.id)
+  end
+
+  it 'should be valid' do
+    expect(@post).to be_valid
+  end
+
+  it 'user id should be present' do
+    @post.user_id = nil
+    expect(@post).not_to be_valid
+  end
+
+  it 'content should be present' do
+    @post.content = '   '
+    expect(@post).not_to be_valid
+  end
+
+  it 'content should be at most 140 characters' do
+    @post.content = 'a' * 141
+    expect(@post).not_to be_valid
+  end
 end
