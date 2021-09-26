d6ea1777782d1a7259b09f5e1d74a90fcb9924ef
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-09-26 15:38:08 +0900

    feat:投稿を若い順に表示させるよう変更

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,6 +1,22 @@
 FactoryBot.define do
   factory :post do
-    content { "MyText" }
-    user { nil }
+    content { 'post test' }
+    created_at { 10.minutes.ago }
+    association :user
+
+    trait :yesterday do
+      content { 'yesterday' }
+      created_at { 1.day.ago }
+    end
+
+    trait :day_before_yesterday do
+      content { 'day_before_yesterday' }
+      created_at { 2.days.ago }
+    end
+
+    trait :now do
+      content { 'now!' }
+      created_at { Time.zone.now }
+    end
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :user do
     name { 'Example User' }
-    email { 'user@example.com' }
+    # email { 'user@example.com' }
+    sequence(:email) { |n| "user#{n}@example.com" }
     password { 'foobar' }
     password_confirmation { 'foobar' }
   end
