2618f93024c5d250113b526baf0842bde011a761
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-09-26 18:35:35 +0900

    fix:投稿一覧表示の準備

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-# users = User.order(:created_at).take(6)
+users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(word_count: 5)
   users.each { |user| user.posts.create!(content: content) }
