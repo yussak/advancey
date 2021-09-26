9a05101528c4fbfe179c8547fed1ef3f3db4b590
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-09-26 21:31:27 +0900

    feat:seedでサンプルユーザ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,20 @@
+# メインのサンプルユーザー
+User.create!(name: 'Yusuke',
+             email: 'rocketpeace1753@gmail.com',
+             password: 'foobar',
+             password_confirmation: 'foobar')
+
+# その他ユーザー作成
+99.times do |n|
+  name = Faker::Name.name
+  email = "user#{n + 1}@example.com"
+  password = 'password'
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password)
+end
+
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(word_count: 5)
