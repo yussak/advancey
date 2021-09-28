339e672b04fdcbe79e446ed0d7cb96e73b597fd6
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-09-29 06:09:23 +0900

    feat:投稿表示の原形追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    # @posts = @user.posts.paginate(page: params[:page])
+    @posts = Post.find(params[:id])
   end
 
   def new
