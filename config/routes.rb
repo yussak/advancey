6dbfc3c124c2874344f73f3ea0ead9e3a2ea6d76
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-09-30 06:50:37 +0900

    postsにリソース追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   get 'sessions/new'
   get 'users/new'
   root 'static_pages#home'
-  get '/about', to:'static_pages#about'
-  get '/contact', to:'static_pages#contact'
-  get '/signup', to:'users#new'
-  get '/login', to:'sessions#new'
-  post '/login', to:'sessions#create'
-  delete'/logout', to:'sessions#destroy'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
+  resources :posts, only: %i[create destroy]
 end
