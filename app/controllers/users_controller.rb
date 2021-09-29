0342c5aa07868b7028cce1aa9e2ba429627448ac
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-09-30 06:44:39 +0900

    feat:投稿複数表示可能にした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = Post.find(params[:id])
+    # @posts = Post.find(params[:id])
+    @posts = @user.posts
     # @posts = @user.posts.paginate(page: params[:page])
   end
 
