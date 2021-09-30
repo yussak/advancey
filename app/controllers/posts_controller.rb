b72dfa3bf5edb97cca79cd429d26d643e1cb31d5
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-10-01 06:42:24 +0900

    feat:自分の投稿のフィードに削除リンク追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :logged_in_user, only: %i[create destroy]
+  before_action :correct_user,   only: :destroy
 
   def create
     @post = current_user.posts.build(post_params)
@@ -13,11 +14,20 @@ class PostsController < ApplicationController
     end
   end
 
-  def destroy; end
+  def destroy
+    @post.destroy
+    flash[:success] = '投稿を削除しました'
+    redirect_to request.referrer || root_url
+  end
 
   private
 
   def post_params
     params.require(:post).permit(:content)
+  end
+
+  def correct_user
+    @post = current_user.posts.find_by(id: params[:id])
+    redirect_to root_url if @post.nil?
   end
 end
