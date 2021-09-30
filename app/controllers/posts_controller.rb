a92bf75b002f3d2735cc0d64b6c510a4e5d5e0ed
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-10-01 05:49:13 +0900

    feat:投稿フォームの原形追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,21 @@
 class PostsController < ApplicationController
   before_action :logged_in_user, only: %i[create destroy]
 
-  def create; end
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:success] = '投稿追加しました！'
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
+  end
 
   def destroy; end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+    @post = current_user.posts.build if logged_in?
   end
 
-  def about
-  end
+  def about; end
 
-  def contact
-  end
+  def contact; end
 end
