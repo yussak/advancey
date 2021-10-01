f3f8ceb7b4290457cf79d404beb1b697e523e1e4
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-10-02 06:26:09 +0900

    test:自分以外の投稿を消せないテスト追加

--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'User controller', type: :request do
   let(:new_post) { FactoryBot.create(:post) }
+  let(:user) { FactoryBot.create(:user) }
 
   it 'should redirect create when not logged in' do
     expect do
@@ -15,5 +16,17 @@ RSpec.describe 'User controller', type: :request do
       delete post_path(:new_post)
     end.to change(Post, :count).by(0)
     expect(response).to redirect_to login_path
+  end
+
+  context 'when logged in user try to delete others post' do
+    let(:user) { FactoryBot.create(:user) }
+    before { log_in_as(user) }
+
+    it 'should redirect destroy for wrong post' do
+      expect do
+        delete post_path(:new_post)
+      end.to change(Post, :count).by(0)
+      expect(response).to redirect_to root_path
+    end
   end
 end
