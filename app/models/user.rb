d8b90844f0e980343df350e44860f36f23c4430a
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-10-05 22:30:36 +0900

    feat:Userモデルにfollowingの関連付けを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: 'Relationship',
                                   foreign_key: 'follower_id',
                                   dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
 
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
