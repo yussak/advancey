2be740d6712748687e94322e14cfc84de635de4f
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-10-15 22:59:59 +0900

    fix:production.rb修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,19 +119,9 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    port: 587,
-    address: 'smtp.gmail.com',
-    domain: 'smtp.gmail.com',
-    user_name: ENV['MAIL_USERNAME'],
-    password: ENV['MAIL_PASSWORD'], # Googleが発行する、12桁のアプリケーションパスワード
-    authentication: :login,
-    enable_starttls_auto: true
-  }
 
+  config.action_mailer.raise_delivery_errors = true
+  ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     port: ENV['MAILGUN_SMTP_PORT'],
     address: ENV['MAILGUN_SMTP_SERVER'],
@@ -140,5 +130,4 @@ Rails.application.configure do
     domain: 'pf-bookworm.herokuapp.com',
     authentication: :plain
   }
-  ActionMailer::Base.delivery_method = :smtp
 end
