f781626ed574a6fc0966d8a252ffa386a3deec0a
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-10-15 23:23:26 +0900

    fix:production.rb修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -122,7 +122,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'pf-bookworm.herokuapp.com'
+  host = 'https://pf-bookworm.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     port: ENV['MAILGUN_SMTP_PORT'],
