6ac40295bf8bc5e9d65a6a633b1dab192a6d5e0a
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-10-16 14:44:57 +0900

    rails generate mailer UserMailer password_reset実行

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: %i[index edit update following followers]
-
   before_action :correct_user,   only: %i[edit update]
 
   def index
