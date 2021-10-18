f4457fb6b70be70e34b6122a73c2ce64ad27a90b
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-10-18 20:19:35 +0900

    test:投稿に画像追加　途中

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,7 +24,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:content)
     params.require(:post).permit(:content, :image)
   end
 
