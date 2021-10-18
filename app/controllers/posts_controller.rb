3597a3e3c090bf322c32bfef3a79b16a614c2120
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-10-18 12:37:16 +0900

    feat:投稿に画像追加可能にした

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,7 @@ class PostsController < ApplicationController
 
   def create
     @post = current_user.posts.build(post_params)
+    @post.image.attach(params[:post][:image])
     if @post.save
       flash[:success] = '投稿を追加しました'
       redirect_to root_url
@@ -24,6 +25,7 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:content)
+    params.require(:post).permit(:content, :image)
   end
 
   def correct_user
