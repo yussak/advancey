178be4b9c67905a811c862e7c0780f954926a1ce
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-10-18 21:53:17 +0900

    feat:画像のバリデーション追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,8 @@ class Post < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  validates :image,   content_type: { in: %w[image/jpeg image/gif image/png],
+                                      message: '適切なフォーマットの画像を追加してください' },
+                      size: { less_than: 5.megabytes,
+                              message: '5MBより小さいものを追加してください' }
 end
