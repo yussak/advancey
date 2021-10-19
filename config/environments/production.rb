bf5473cc5948cc0fb33f216677448c247eb03ac3
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-10-19 09:10:24 +0900

    S3を使うよう設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,9 +39,6 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
-
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
@@ -119,6 +116,9 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  # アップロードされたファイルをAWSに保存する
+  config.active_storage.service = :amazon
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
