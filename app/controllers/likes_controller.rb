d36c25d438af178cdb8f409241bd192e3d079b1d
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-10-26 17:39:45 +0900

    いいね一覧を表示可能にした

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,14 +3,12 @@ class LikesController < ApplicationController
     current_user.like_this(clicked_post)
     flash[:success] = '投稿に「いいね」しました。'
     redirect_back(fallback_location: root_path)
-    # @like_count = Like.where(like_id: params[:like_id]).count
   end
 
   def destroy
     current_user.likes.find_by(post_id: params[:post_id]).destroy
     flash[:success] = '「いいね」を解除しました。'
     redirect_back(fallback_location: root_path)
-    # @like_count = Like.where(like_id: params[:like_id]).count
   end
 
   private
