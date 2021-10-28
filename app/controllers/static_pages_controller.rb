0edc8798f97bfc1f8385197c6630464d985d60aa
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-10-28 18:51:17 +0900

    投稿フィードにページネーション追加

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,8 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @post = current_user.posts.build
-      # @feed_items = current_user.feed.paginate(page: params[:page])
-      @feed_items = current_user.feed
+      @feed_items = current_user.feed.paginate(page: params[:page], per_page: 5)
     end
   end
 
