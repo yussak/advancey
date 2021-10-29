53ce0acdbf874181966af97fdccdb3372f15e5ec
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-10-30 04:17:19 +0900

    サイドバーのレイアウト調整

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
+    # @user = User.find(params[:id])
     if logged_in?
       @post = current_user.posts.build
       @feed_items = current_user.feed.paginate(page: params[:page], per_page: 5)
