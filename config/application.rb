718184271cd9c6124fdece25d48f723e52681d1b
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-10-31 22:47:51 +0900

    kaminari 日本語対応

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module PortfolioApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    # rails デフォルトを日本語にする
+    config.i18n.default_locale = :ja
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
