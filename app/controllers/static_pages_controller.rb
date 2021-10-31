58f334f96ab650a1dadf48145fe0129851534ff1
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-10-31 22:06:47 +0900

    kaminari bootstrap4適用

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,8 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @post = current_user.posts.build
-      # @feed_items = current_user.feed.paginate(page: params[:page], per_page: 5)
-      @feed_items = current_user.feed.page params[:page]
+      @feed_items = current_user.feed.page params[:page].per(3)
     end
   end
 
