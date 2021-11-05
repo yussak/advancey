c73a80201ba67b4a5b2e2724526d3c3fa3c98839
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-11-05 19:29:10 +0900

    ログイン時フラッシュ追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     if user&.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      flash[:success] = 'ログインしました'
       redirect_back_or user
     else
       flash.now[:danger] = 'メールアドレスまたはパスワードが間違っています'
