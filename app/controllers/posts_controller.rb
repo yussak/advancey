08923eeb5eb1f764424bed7f50777c4ec99d7a9e
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-11-11 20:57:51 +0900

    投稿タグレイアウト・コード調整

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -43,7 +43,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    # params.require(:post).permit(:content, :image)
     params.require(:post).permit(:content, :image, :action)
   end
 
