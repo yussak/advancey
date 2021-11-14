bca5486895e0c842de54100e9a0e7459b1f7b527
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-11-14 23:22:00 +0900

    ログイン後のリダイレクトをトップページに変更

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       flash[:success] = 'ログインしました'
-      redirect_back_or user
+      redirect_back_or root_url
     else
       flash.now[:danger] = 'メールアドレスまたはパスワードが間違っています'
       render 'new'
