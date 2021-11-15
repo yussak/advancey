d2f3bd65e07a9e2ed62b879452c49ef435979c88
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-11-16 00:01:53 +0900

    プロフィールフォーム実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -81,7 +81,8 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :profile)
+    # params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
   def correct_user
