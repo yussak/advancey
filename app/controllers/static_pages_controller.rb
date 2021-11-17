61fd79ab5275ecc7949fbea02799e0be635448d6
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-11-17 14:00:48 +0900

    homeページ　ユーザーのいいね一覧の処理をビューからコントローラーに書き換えた

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,7 @@ class StaticPagesController < ApplicationController
       @post = current_user.posts.build
       @feed_items = current_user.feed.page(params[:page]).per(5)
       @user_posts = current_user.posts.page(params[:page]).per(5)
+      @like_posts = current_user.like_posts.page(params[:page]).per(5)
     end
   end
 
