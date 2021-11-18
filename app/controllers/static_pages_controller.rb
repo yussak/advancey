ecf6e2041bf706bdecae7548efdf1e428fe9bb91
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-11-18 20:44:31 +0900

    homeページ　ページネーション削除

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,9 +2,9 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @post = current_user.posts.build
-      @feed_items = current_user.feed.page(params[:page]).per(5)
-      @user_posts = current_user.posts.page(params[:page]).per(5)
-      @like_posts = current_user.like_posts.page(params[:page]).per(5)
+      @feed_items = current_user.feed.page(params[:page])
+      @user_posts = current_user.posts.page(params[:page])
+      @like_posts = current_user.like_posts.page(params[:page])
     end
   end
 
