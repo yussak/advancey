1d0b9d1ca483c56e3e301b88927d8027c70285e3
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-11-22 00:47:20 +0900

    コメント非同期で追加・削除可能にした

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,27 +1,52 @@
 class CommentsController < ApplicationController
   def create
+    @post = Post.find(params[:post_id])
+
     @comment = current_user.comments.new(comment_params)
-    @comment.post_id = params[:post_id]
+    # @comment.post_id = params[:post_id]
+    @comment.post_id = @post.id
+
     if @comment.save
-      flash[:success] = 'コメントを追加しました'
-      redirect_back(fallback_location: root_path)
+      flash.now[:success] = 'コメントを追加しました'
+      render :index
+
+    # redirect_to post_path(@post)
+    # flash[:success] = 'コメントを追加しました'
+    # redirect_back(fallback_location: root_path)
+    # respond_to do |format|
+    #   # format.html { redirect_to post_path }
+    #   format.html
+    #   format.js
+    # end
     else
-      @post = Post.find(params[:post_id])
-      @comments = @post.comments
-      render 'posts/show'
+      # @post = Post.find(params[:post_id])
+      # @comments = @post.comments
+      # render 'posts/show'
+      render :error
     end
   end
 
   def destroy
-    @comment = Comment.find(params[:id])
-    if @comment.destroy
-      flash[:success] = 'コメントを削除しました'
-      redirect_back(fallback_location: root_path)
-    else
-      flash.now[:danger] = 'コメント削除に失敗しました'
-      # ↓多分使えない
-      render post_path(@post)
-    end
+    # @comment = Comment.find(params[:id])
+    @comment = Comment.find_by(id: params[:id], post_id: params[:post_id])
+    @comment.destroy
+    flash.now[:alert] = '投稿を削除しました'
+    @post = Post.find(params[:post_id])
+    render :index # render先にjsファイルを指定
+
+    # if @comment.destroy
+    # flash[:success] = 'コメントを削除しました'
+    # redirect_back(fallback_location: root_path)
+    # respond_to do |format|
+    #   # format.html { redirect_to post_path }
+    #   format.html
+    #   format.js
+    # end
+    # else
+    #   flash.now[:danger] = 'コメント削除に失敗しました'
+    #   # ↓多分使えない
+    #   # render post_path(@post)
+    # end
   end
 
   private
