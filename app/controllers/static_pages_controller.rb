dd03299ad796e6798d86bd56a1ef018f3333db05
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-11-23 22:10:33 +0900

    投稿タブを一つのパーシャルから読み込みに変更

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,9 +2,15 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @post = current_user.posts.build
-      # page不要なら削除
-      @feed_items = current_user.feed.page(params[:page])
+      @all_posts = current_user.feed # 自分・フォロー中の投稿
+
+      # page消したい
       @user_posts = current_user.posts.page(params[:page])
+      # @user_posts = current_user.posts
+
+      @want_posts = current_user.posts.where(action: '実践したい')
+      @doing_posts = current_user.posts.where(action: '実践中')
+      @master_posts = current_user.posts.where(action: '身についた')
       @like_posts = current_user.like_posts
     end
   end
