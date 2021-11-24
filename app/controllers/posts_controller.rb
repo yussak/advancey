f5fc5d5cf5854d7763a5f32e4fd63ec97c4ffca4
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-11-24 20:14:59 +0900

    投稿ajax対応（追加のみ）

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,25 +5,28 @@ class PostsController < ApplicationController
   def create
     @post = current_user.posts.build(post_params)
     @post.image.attach(params[:post][:image])
-    if @post.save
-      flash[:success] = '投稿を追加しました'
-      redirect_to root_url
-    else
-      # render 'static_pages/home'するため
-      @all_posts = current_user.feed
-      @user_posts = current_user.posts.all
-      @want_posts = current_user.posts.where(action: '実践したい')
-      @doing_posts = current_user.posts.where(action: '実践中')
-      @master_posts = current_user.posts.where(action: '身についた')
-      @like_posts = current_user.like_posts
-      render 'static_pages/home'
-    end
+    # if @post.save
+    #   flash[:success] = '投稿を追加しました'
+    #   # redirect_to root_url
+    # else
+    #   # render 'static_pages/home'するため
+    #   @user_posts = current_user.posts.all
+    #   @want_posts = current_user.posts.where(action: '実践したい')
+    #   @doing_posts = current_user.posts.where(action: '実践中')
+    #   @master_posts = current_user.posts.where(action: '身についた')
+    #   @like_posts = current_user.like_posts
+    #   # render 'static_pages/home' #ajaxのため消してみる
+
+    # 重すぎるので一時的に数制限
+    @all_posts = current_user.feed.limit(5)
+    # end
+    flash[:success] = '投稿を追加しました' if @post.save
   end
 
   def destroy
     @post.destroy
     flash[:success] = '投稿を削除しました'
-    redirect_to root_url
+    # redirect_to root_url
   end
 
   def edit
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,12 +2,15 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @post = current_user.posts.build
-      @all_posts = current_user.feed # 自分・フォロー中の投稿
-      @user_posts = current_user.posts.all # 自分の投稿
-      @want_posts = current_user.posts.where(action: '実践したい')
-      @doing_posts = current_user.posts.where(action: '実践中')
-      @master_posts = current_user.posts.where(action: '身についた')
-      @like_posts = current_user.like_posts
+      # @all_posts = current_user.feed # 自分・フォロー中の投稿
+      # @user_posts = current_user.posts.all # 自分の投稿
+      # @want_posts = current_user.posts.where(action: '実践したい')
+      # @doing_posts = current_user.posts.where(action: '実践中')
+      # @master_posts = current_user.posts.where(action: '身についた')
+      # @like_posts = current_user.like_posts
+
+      # 重すぎるので一時的に数制限
+      @all_posts = current_user.feed.limit(5)
     end
   end
 
