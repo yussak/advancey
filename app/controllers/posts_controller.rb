587b945a4c56dbee4a567277f922aad9ce66771f
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-11-30 05:48:13 +0900

    投稿追加の個別ページ作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,10 @@ class PostsController < ApplicationController
   before_action :logged_in_user, only: %i[create new edit update destroy]
   before_action :correct_user,   only: :destroy
 
+  def new
+    @post = Post.new
+  end
+
   def create
     @post = current_user.posts.build(post_params)
     @post.image.attach(params[:post][:image])
