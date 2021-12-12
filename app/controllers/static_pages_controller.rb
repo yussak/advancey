51014dd8b10466162a98c9defeab80449955e348
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-12-13 03:04:55 +0900

    TOP 無限スクロール　タブ切り替え時発火（全部の投稿は切り替えない動作しない）

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,16 +2,25 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @post = current_user.posts.build
-      @all_posts = current_user.feed.page(params[:page]).per(9)
-      @user_posts = current_user.posts.page(params[:page]).per(9)
-      @want_posts = current_user.posts.where(action: '実践したい').page(params[:page]).per(9)
-      @doing_posts = current_user.posts.where(action: '実践中').page(params[:page]).per(9)
-      @master_posts = current_user.posts.where(action: '身についた').page(params[:page]).per(9)
-      # エラー出るので一時的に並び替えたもの↓
-      @like_posts = current_user.like_posts.page(params[:page]).per(9)
+      # @all_posts = current_user.feed.page(params[:page]).per(9)
+      # @user_posts = current_user.posts.page(params[:page]).per(9)
+      # @want_posts = current_user.posts.where(action: '実践したい').page(params[:page]).per(9)
+      # @doing_posts = current_user.posts.where(action: '実践中').page(params[:page]).per(9)
+      # @master_posts = current_user.posts.where(action: '身についた').page(params[:page]).per(9)
+      # # エラー出るので一時的に並び替えたもの↓
+      # @like_posts = current_user.like_posts.page(params[:page]).per(9)
 
       # # いいねをつけた順に表示
       # @like_posts = current_user.likes.order(created_at: 'DESC').map { |like| like.post }
+
+      # 無限スクロール修正　一時的
+      @all_posts = current_user.feed.page(params[:page]).per(3)
+      @user_posts = current_user.posts.page(params[:page]).per(3)
+      @want_posts = current_user.posts.where(action: '実践したい').page(params[:page]).per(3)
+      @doing_posts = current_user.posts.where(action: '実践中').page(params[:page]).per(3)
+      @master_posts = current_user.posts.where(action: '身についた').page(params[:page]).per(3)
+      # エラー出るので一時的に並び替えたもの↓
+      @like_posts = current_user.like_posts.page(params[:page]).per(3)
     end
   end
 
