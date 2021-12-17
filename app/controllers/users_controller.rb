b82b3ef552fd0bffec4000cd6457057b5a23b7ca
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2021-12-18 00:16:09 +0900

    新規登録時TOPにリダイレクトに変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,7 @@ class UsersController < ApplicationController
     if @user.save
       log_in @user
       flash[:success] = 'Bookwormへようこそ！'
-      redirect_to @user
+      redirect_to root_path
     else
       render 'new'
     end
