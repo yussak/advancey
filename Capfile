eb8dad77073df992177862e048126c688c1e6dc6
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-01-04 15:54:43 +0900

    AWS自動デプロイの準備

---
+++ Capfile
@@ -0,0 +1,9 @@
+require 'capistrano/setup'
+require 'capistrano/deploy'
+require 'capistrano/rbenv'
+require 'capistrano/bundler'
+require 'capistrano/rails/packs'
+require 'capistrano/rails/migrations'
+require 'capistrano3/puma'
+
+Dir.glob('lib/capistrano/tasks/*.rake').each { |r| import r }
---
+++ config/deploy.rb
@@ -0,0 +1,37 @@
+# config valid only for current version of Capistrano
+# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
+lock '3.16.0'
+
+# Capistranoのログの表示に利用する
+set :application, 'bookworm'
+set :deploy_to, '/var/www/portfolio_app'
+
+# どのリポジトリからアプリをpullするかを指定する
+set :repo_url,  'git@github.com:YusukeSakuraba/portfolio_app.git'
+
+# バージョンが変わっても共通で参照するディレクトリを指定
+set :linked_dirs,
+    fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system',
+                                 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '3.0.2'
+
+# どの公開鍵を利用してデプロイするか
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/pf-keypair.pem']
+
+# プロセス番号を記載したファイルの場所
+set :puma_pid, -> { "#{shared_path}/tmp/pids/puma.pid" }
+
+# pumaの設定ファイルの場所
+set :puma_config_path, -> { "#{current_path}/config/puma.rb" }
+set :keep_releases, 5
+
+# デプロイ処理が終わった後、pumaを再起動するための記述
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'puma:restart'
+  end
+end
