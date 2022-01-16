9888236883df23d54be3a8be57514295cc23ff22
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-01-16 22:40:36 +0900

    [fix]TOP・プロフィールページに非公開にした投稿を表示しないようにした（実践中の投稿）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @user_posts = @user.posts.where(privacy: false).page(params[:page]).per(9)
     @want_posts = @user.posts.where(privacy: false).where(tag: '実践したい').page(params[:page]).per(9)
-    @doing_posts = @user.posts.where(tag: '実践中').page(params[:page]).per(9)
+    @doing_posts = @user.posts.where(privacy: false).where(tag: '実践中').page(params[:page]).per(9)
     @master_posts = @user.posts.where(tag: '身についた').page(params[:page]).per(9)
     # ↓なんかエラー出るので一時的に並び替え解除
     @like_posts = @user.like_posts.page(params[:page]).per(9)
