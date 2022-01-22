3622891168e3ca7dbe843c2d1bdb7c6e3fd6bc9f
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-01-23 00:35:46 +0900

    [fix]vue-loader使用するよう設定

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,14 +1,15 @@
 const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
-const vue = require('./loaders/vue')
 
-const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-    new webpack.ProvidePlugin({
-        $: 'jquery/src/jquery',
-        jQuery: 'jquery/src/jquery'
-    })
+environment.plugins.prepend(
+    'VueLoaderPlugin',
+    new VueLoaderPlugin()
 )
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-environment.loaders.prepend('vue', vue)
+
+environment.loaders.prepend('vue', {
+    test: /\.vue$/,
+    use: [{
+        loader: 'vue-loader'
+    }]
+})
 module.exports = environment
