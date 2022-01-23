383c220d03d175d41d8848a40d9eaea0d9ccc6b8
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-01-24 04:19:55 +0900

    [fix]jqueryが動かないのを修正

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -12,4 +12,14 @@ environment.loaders.prepend('vue', {
         loader: 'vue-loader'
     }]
 })
+
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+    new webpack.ProvidePlugin({
+        $: 'jquery/src/jquery',
+        jQuery: 'jquery/src/jquery'
+    })
+)
+
+
 module.exports = environment
