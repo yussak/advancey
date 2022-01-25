1aa10b9ab073073e06010a8dcd5555553d13b9cf
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-01-25 12:14:02 +0900

    [fix]Topicコメント 文字数制限追加

--- app/models/topic_comment.rb
+++ app/models/topic_comment.rb
@@ -2,6 +2,5 @@ class TopicComment < ApplicationRecord
   belongs_to :user
   belongs_to :topic
 
-  validates :topic_comment_content, presence: true
-  # validates :title, presence: true, length: { maximum: 20 }
+  validates :topic_comment_content, presence: true, length: { maximum: 100 } # 仮
 end
