88a39dbe35de991a0f894c765c9f30816100439c
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-01-25 11:36:08 +0900

    [fix]Topic title 文字数制限追加

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,5 +2,5 @@ class Topic < ApplicationRecord
   belongs_to :user
   has_many :topic_comments
 
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 20 }
 end
