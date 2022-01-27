65294a5a3f7f4fb0606c4c22289482a868400e92
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-01-27 23:49:21 +0900

    AWSデプロイのため

--- app/javascript/packs/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from "vue";
-import App from "../app.vue";
-import router from './router';
-
-document.addEventListener("DOMContentLoaded", () => {
-createApp(App).use(router).mount('#vue-app')
-});
