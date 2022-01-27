56098eae02d0477d89e72be78c78ae12f87d46ab
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-01-27 21:09:27 +0900

    AWSデプロイのため

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,6 @@
 require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
-
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -16,12 +14,8 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
-  # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+  config.require_master_key = true
 
-  # Disable serving static files from the `/public` folder by default since
-  # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress CSS using a preprocessor.
@@ -123,7 +117,8 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://pf-bookworm.herokuapp.com'
+  # host = 'https://pf-bookworm.herokuapp.com'
+  host = 'https://pf-bookworm.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     port: ENV['MAILGUN_SMTP_PORT'],
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,19 +1,17 @@
 const { environment } = require('@rails/webpacker')
-const { VueLoaderPlugin } = require('vue-loader')
-
-environment.plugins.prepend(
-    'VueLoaderPlugin',
-    new VueLoaderPlugin()
-)
-
-environment.loaders.prepend('vue', {
-    test: /\.vue$/,
-    use: [{
-        loader: 'vue-loader'
-    }]
-})
-
 const webpack = require('webpack')
+// const { VueLoaderPlugin } = require('vue-loader')
+
+// environment.plugins.prepend(
+//     'VueLoaderPlugin',
+//     new VueLoaderPlugin()
+// )
+
+// environment.loaders.prepend('vue', {
+//     test: /\.vue$/,
+//     use: [{loader: 'vue-loader']
+// })
+
 environment.plugins.prepend('Provide',
     new webpack.ProvidePlugin({
         $: 'jquery/src/jquery',
