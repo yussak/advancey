38fa021bbf033fe898ebbbad7d981ad73504f8e1
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-01-29 22:39:32 +0900

    CircleCIからAWSへ自動デプロイのため

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,8 +1,8 @@
 server '54.65.215.44', user: 'yusuke', roles: %w[app db web]
 
 set :ssh_options, {
-  # keys: %w[~/.ssh/pf-keypair.pem],
-  keys: [ENV.fetch('PRODUCTION_SSH_KEY').to_s],
+  keys: %w[~/.ssh/pf-keypair.pem],
+  # keys: [ENV.fetch('PRODUCTION_SSH_KEY').to_s],
   forward_agent: true,
   auth_methods: %w[publickey]
 }
