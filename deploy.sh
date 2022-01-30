d0dbcb1a03099478d3e3bfb5ed7695b9ee31a769
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-01-30 17:28:42 +0900

    AWS自動デプロイでセキュリティグループの変更が戻らないのを修正するため

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,10 @@ set -ex
 SECURITY_GROUP="sg-043710d377121b44c" #セキュリティグループのID
 IP=`curl -s ifconfig.me`  # 現在起動しているコンテナのIPアドレスを代入
 
-trap "aws ec2 revoke-security-group-ingress --group-id ${SECURITY_GROUP} --protocol tcp --port 22 --cidr ${IP}/32" 0 1 2 3 15
 # コンテナのIPアドレスからのsshをauthorize(許可する)
 aws ec2 authorize-security-group-ingress --group-id ${SECURITY_GROUP} --protocol tcp --port 22 --cidr ${IP}/32
+
+trap "aws ec2 revoke-security-group-ingress --group-id ${SECURITY_GROUP} --protocol tcp --port 22 --cidr ${IP}/32" 0 1 2 3 15
 bundle exec cap production deploy
 # 許可した設定をrevoke(取り消す)
 # aws ec2 revoke-security-group-ingress --group-id ${SECURITY_GROUP} --protocol tcp --port 22 --cidr ${IP}/32
