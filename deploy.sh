433ead21d2bc962a4cb8b2c2236952d66992c6d5
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-01-30 11:42:40 +0900

    CircleCIからAWSデプロイ時IP制限ありに戻すため修正(sg戻す・不要コマンド削除)

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,8 @@
 set -ex
 
 
-SECURITY_GROUP="sg-0980635bb414c35d3" #セキュリティグループのID
+SECURITY_GROUP="sg-03d023d57020199b9" #セキュリティグループのID
 IP=`curl -s ifconfig.me`  # 現在起動しているコンテナのIPアドレスを代入
-sudo ntpdate -v ntp.nict.jp
 
 # コンテナのIPアドレスからのsshをauthorize(許可する)
 aws ec2 authorize-security-group-ingress --group-id ${SECURITY_GROUP} --protocol tcp --port 22 --cidr ${IP}/32
