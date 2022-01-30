5a3d6cb20a60ff441bbce93d91cef63d01c5103d
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-01-30 11:48:31 +0900

    CircleCIからAWSデプロイ時IP制限ありに戻すため修正(IAMポリシー編集)

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ set -ex
 SECURITY_GROUP="sg-03d023d57020199b9" #セキュリティグループのID
 IP=`curl -s ifconfig.me`  # 現在起動しているコンテナのIPアドレスを代入
 
+
 # コンテナのIPアドレスからのsshをauthorize(許可する)
 aws ec2 authorize-security-group-ingress --group-id ${SECURITY_GROUP} --protocol tcp --port 22 --cidr ${IP}/32
 bundle exec cap production deploy
