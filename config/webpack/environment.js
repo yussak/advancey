7ce4e2b711f425cbced8a89ef4257b677d23932e
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-01-30 14:22:23 +0900

    [feat]Vue導入・webpack設定

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,18 +1,18 @@
 const { environment } = require('@rails/webpacker')
 const webpack = require('webpack')
-// const { VueLoaderPlugin } = require('vue-loader')
+const { VueLoaderPlugin } = require('vue-loader')
 
-// environment.plugins.prepend(
-//     'VueLoaderPlugin',
-//     new VueLoaderPlugin()
-// )
+environment.plugins.prepend(
+    'VueLoaderPlugin',
+    new VueLoaderPlugin()
+)
 
-// environment.loaders.prepend('vue', {
-//     test: /\.vue$/,
-//     use: [{
-//       loader: 'vue-loader'
-//   }]
-// })
+environment.loaders.prepend('vue', {
+    test: /\.vue$/,
+    use: [{
+      loader: 'vue-loader'
+  }]
+})
 
 environment.plugins.prepend('Provide',
     new webpack.ProvidePlugin({
