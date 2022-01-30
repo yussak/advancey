4e163c6007475ddd4e8f532002e08132d8f18619
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-01-30 16:46:39 +0900

    Vueがインストール出来ないのを修正したい

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -2,17 +2,4 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
-const { VueLoaderPlugin } = require('vue-loader')
-environment.plugins.prepend(
-  'VueLoaderPlugin',
-  new VueLoaderPlugin()
-)
-
-environment.loaders.prepend('vue', {
-  test: /\.vue$/,
-  use: [{
-    loader: 'vue-loader'
-}]
-})
-
 module.exports = environment.toWebpackConfig()
