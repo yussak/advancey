503d13526cf54c24aac86bb53534567e23b464f1
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-01-30 20:52:24 +0900

    Vue表示するため修正

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,24 +2,24 @@ const { environment } = require('@rails/webpacker')
 const webpack = require('webpack')
 const { VueLoaderPlugin } = require('vue-loader')
 
-environment.plugins.prepend(
-    'VueLoaderPlugin',
-    new VueLoaderPlugin()
-)
 
 environment.loaders.prepend('vue', {
-    test: /\.vue$/,
-    use: [{
-      loader: 'vue-loader'
+  test: /\.vue$/,
+  use: [{
+    loader: 'vue-loader'
   }]
 })
 
 environment.plugins.prepend('Provide',
-    new webpack.ProvidePlugin({
-        $: 'jquery/src/jquery',
-        jQuery: 'jquery/src/jquery'
-    })
+new webpack.ProvidePlugin({
+  $: 'jquery/src/jquery',
+  jQuery: 'jquery/src/jquery'
+})
 )
 
+environment.plugins.prepend(
+    'VueLoaderPlugin',
+    new VueLoaderPlugin()
+)
 
 module.exports = environment
