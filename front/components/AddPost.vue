2de18806ca19050868a1b4d009f50b3921042cdb
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-02-03 20:24:27 +0900

    [feat]コンポーネントで投稿追加、一覧表示可能にした

---
+++ front/components/AddPost.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-form>
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="4">
+          <v-text-field
+            v-model="content"
+            counter="10"
+            label="post"
+            required
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12" md="4">
+          <v-btn @click="handleSubmit">作成</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      content: "",
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.$emit("submit", this.content);
+      this.content = "";
+    },
+  },
+};
+</script>
+
+<style>
+</style>
+
---
+++ front/components/PostList.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-card>
+    <v-card-content>
+      投稿一覧
+      <v-spacer></v-spacer>
+      <v-text-field
+        v-model="search"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    </v-card-content>
+    <v-data-table
+      :headers="headers"
+      :items="posts"
+      :search="search"
+    ></v-data-table>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ["posts"],
+  data() {
+    return {
+      search: "",
+      headers: [
+        {
+          text: "タイトル",
+          align: "left",
+          sortable: false,
+          value: "content",
+        },
+        { text: "ユーザー名", value: "username" },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
