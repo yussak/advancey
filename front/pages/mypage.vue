93fe3aa12fe8881e8b5040a8478408049af94d85
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-02-26 13:33:17 +0900

    [style]マイページ　レイアウト調整

--- front/pages/mypage.vue
+++ front/pages/mypage.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
     <div v-if="user">
-      <p>Email: {{ user.email }}</p>
-      <p>名前: {{ user.name }}</p>
+      <h2 class="text-center">
+        <span style="color: red; font-weight: normal; font-size: 20px"
+          >ここにアイコン画像</span
+        >{{ user.name }}さん
+      </h2>
+      <p>フォロー中：</p>
+      <p>フォロワー：</p>
+      <!-- 編集画面で表示させる・マイページには出さない -->
+      <!-- <p>email: {{ user.email }}</p> -->
     </div>
-    <v-btn @click="logOut">LOGOUT</v-btn>
-    <v-btn @click="deleteUser">deleteUser</v-btn>
+    <!-- 編集画面で表示させる・マイページには出さない -->
+    <!-- <v-btn @click="logOut">ログアウト</v-btn> -->
+    <!-- <v-btn @click="deleteUser">退会する</v-btn> -->
   </div>
 </template>
 
