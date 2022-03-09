4f8e97f15e193c8ca719b63c1227d8db5b6e1822
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-03-10 06:18:55 +0900

    デフォルトアイコン追加・メモ削除時フラッシュ追加・バグ修正

--- front/components/PostList.vue
+++ front/components/PostList.vue
@@ -15,11 +15,13 @@
               <v-col v-for="post in posts" :key="post.id" :cols="6">
                 <!-- 新しいのが下に追加されるので修正したい -->
                 <v-card>
-                  <!-- サンプル画像 -->
-                  <img
-                    src="~assets/test.jpg"
-                    style="width: 45px; height: 45px; border-radius: 50%"
-                  />
+                  <v-avatar>
+                    <!-- アイコン設定がないとき→条件は後で追加 -->
+                    <img
+                      src="~assets/default-user-icon.png"
+                      style="width: 45px; height: 45px"
+                    />
+                  </v-avatar>
                   <v-card-text>
                     <v-row>
                       <p>
@@ -65,118 +67,120 @@
           </v-container>
         </v-tab-item>
         <!-- みやすさのため一時的にdiv追加(閉じれるので) -->
-        <div>
-          <v-tab-item>
-            <v-card>
-              <v-container fluid>
-                <v-row dense>
-                  <!-- 空のときテキスト表示したい -->
-                  <v-col v-for="post in posts" :key="post.id" :cols="6">
-                    <!-- 新しいのが下に追加されるので修正したい -->
-                    <v-card>
-                      <!-- サンプル画像 -->
+        <v-tab-item>
+          <v-card>
+            <v-container fluid>
+              <v-row dense>
+                <!-- 空のときテキスト表示したい -->
+                <v-col v-for="post in posts" :key="post.id" :cols="6">
+                  <!-- 新しいのが下に追加されるので修正したい -->
+                  <v-card>
+                    <v-avatar>
+                      <!-- アイコン設定がないとき→条件は後で追加 -->
                       <img
-                        src="~assets/test.jpg"
-                        style="width: 45px; height: 45px; border-radius: 50%"
+                        src="~assets/default-user-icon.png"
+                        style="width: 45px; height: 45px"
                       />
-                      <v-card-text>
-                        <v-row>
-                          <p>
-                            <span style="font-weight: bold"
-                              >{{ post.username }}さん</span
-                            >が
-                          </p>
-                          <p>{{ post.created_at }}に投稿</p>
-                        </v-row>
-                      </v-card-text>
-                      <v-card-title v-text="post.content"></v-card-title>
-                      <v-card-text>
-                        <v-row>
-                          <!-- 後で消す→非公開の投稿自体ここに表示しないので -->
-                          <p
-                            v-if="post.privacy === true"
-                            style="color: red; font-weight: bold"
-                          >
-                            Private
-                          </p>
-                          <v-icon @click="deleteItem(post)">delete</v-icon>
-                          <v-icon @click="showItem(post)">mdi-magnify</v-icon>
-                          <!-- タグがある時だけアイコン表示 -->
-                          <p v-if="post.tag !== ''">
-                            <v-icon>mdi-tag</v-icon>{{ post.tag }}
-                          </p>
-                        </v-row>
-                      </v-card-text>
-                    </v-card>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-card>
-          </v-tab-item>
-          <v-tab-item>
-            <v-card>
-              <v-container fluid>
-                <v-row dense>
-                  <!-- 空のときテキスト表示したい -->
-                  <!-- <v-col v-for="post in doi" :key="post.id" :cols="6"> -->
-                  <v-col v-for="post in posts" :key="post.id" :cols="6">
-                    <!-- 新しいのが下に追加されるので修正したい -->
-                    <v-card>
-                      <!-- サンプル画像 -->
+                    </v-avatar>
+                    <v-card-text>
+                      <v-row>
+                        <p>
+                          <span style="font-weight: bold"
+                            >{{ post.username }}さん</span
+                          >が
+                        </p>
+                        <p>{{ post.created_at }}に投稿</p>
+                      </v-row>
+                    </v-card-text>
+                    <v-card-title v-text="post.content"></v-card-title>
+                    <v-card-text>
+                      <v-row>
+                        <!-- 後で消す→非公開の投稿自体ここに表示しないので -->
+                        <p
+                          v-if="post.privacy === true"
+                          style="color: red; font-weight: bold"
+                        >
+                          Private
+                        </p>
+                        <v-icon @click="deleteItem(post)">delete</v-icon>
+                        <v-icon @click="showItem(post)">mdi-magnify</v-icon>
+                        <!-- タグがある時だけアイコン表示 -->
+                        <p v-if="post.tag !== ''">
+                          <v-icon>mdi-tag</v-icon>{{ post.tag }}
+                        </p>
+                      </v-row>
+                    </v-card-text>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card>
+        </v-tab-item>
+        <v-tab-item>
+          <v-card>
+            <v-container fluid>
+              <v-row dense>
+                <!-- 空のときテキスト表示したい -->
+                <!-- <v-col v-for="post in doi" :key="post.id" :cols="6"> -->
+                <v-col v-for="post in posts" :key="post.id" :cols="6">
+                  <!-- 新しいのが下に追加されるので修正したい -->
+                  <v-card>
+                    <v-avatar>
+                      <!-- アイコン設定がないとき→条件は後で追加 -->
                       <img
-                        src="~assets/test.jpg"
-                        style="width: 45px; height: 45px; border-radius: 50%"
+                        src="~assets/default-user-icon.png"
+                        style="width: 45px; height: 45px"
                       />
-                      <v-card-text>
-                        <v-row>
-                          <p>
-                            <span style="font-weight: bold"
-                              >{{ post.username }}さん</span
-                            >が
-                          </p>
-                          <p>{{ post.created_at }}に投稿</p>
-                        </v-row>
-                      </v-card-text>
-                      <v-card-title v-text="post.content"></v-card-title>
-                      <v-card-text>
-                        <v-row>
-                          <!-- 後で消す→非公開の投稿自体ここに表示しないので -->
-                          <p
-                            v-if="post.privacy === true"
-                            style="color: red; font-weight: bold"
-                          >
-                            Private
-                          </p>
-                          <v-icon @click="deleteItem(post)">delete</v-icon>
-                          <v-icon @click="showItem(post)">mdi-magnify</v-icon>
-                          <!-- タグがある時だけアイコン表示 -->
-                          <p v-if="post.tag !== ''">
-                            <v-icon>mdi-tag</v-icon>{{ post.tag }}
-                          </p>
-                        </v-row>
-                      </v-card-text>
-                    </v-card>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-card>
-          </v-tab-item>
-          <v-tab-item>
-            <v-card>
-              <v-card-text> タブ4の内容をここに記述します。 </v-card-text>
-            </v-card>
-          </v-tab-item>
-          <v-tab-item>
-            <v-card>
-              <v-card-text> タブ5の内容をここに記述します。 </v-card-text>
-            </v-card>
-          </v-tab-item>
-          <v-tab-item>
-            <v-card>
-              <v-card-text> タブ6の内容をここに記述します。 </v-card-text>
-            </v-card>
-          </v-tab-item>
-        </div>
+                    </v-avatar>
+                    <v-card-text>
+                      <v-row>
+                        <p>
+                          <span style="font-weight: bold"
+                            >{{ post.username }}さん</span
+                          >が
+                        </p>
+                        <p>{{ post.created_at }}に投稿</p>
+                      </v-row>
+                    </v-card-text>
+                    <v-card-title v-text="post.content"></v-card-title>
+                    <v-card-text>
+                      <v-row>
+                        <!-- 後で消す→非公開の投稿自体ここに表示しないので -->
+                        <p
+                          v-if="post.privacy === true"
+                          style="color: red; font-weight: bold"
+                        >
+                          Private
+                        </p>
+                        <v-icon @click="deleteItem(post)">delete</v-icon>
+                        <v-icon @click="showItem(post)">mdi-magnify</v-icon>
+                        <!-- タグがある時だけアイコン表示 -->
+                        <p v-if="post.tag !== ''">
+                          <v-icon>mdi-tag</v-icon>{{ post.tag }}
+                        </p>
+                      </v-row>
+                    </v-card-text>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card>
+        </v-tab-item>
+        <v-tab-item>
+          <v-card>
+            <v-card-text> タブ4の内容をここに記述します。 </v-card-text>
+          </v-card>
+        </v-tab-item>
+        <v-tab-item>
+          <v-card>
+            <v-card-text> タブ5の内容をここに記述します。 </v-card-text>
+          </v-card>
+        </v-tab-item>
+        <v-tab-item>
+          <v-card>
+            <v-card-text> タブ6の内容をここに記述します。 </v-card-text>
+          </v-card>
+        </v-tab-item>
       </v-tabs>
     </v-card>
   </div>
@@ -277,7 +281,6 @@ export default {
       this.$router.push(`/posts/${item.id}`);
     },
     async deleteItem(item) {
-      // 削除フラッシュ表示する（成功・失敗時）
       const res = confirm("本当に削除しますか？");
       if (res) {
         await axios.delete(`/v1/posts/${item.id}`);
@@ -288,6 +291,15 @@ export default {
           ...this.user,
           posts,
         };
+        // 削除フラッシュ表示する（成功時）
+        this.$store.dispatch("notification/setNotice", {
+          status: true,
+          message: "メモを削除しました",
+        });
+        setTimeout(() => {
+          this.$store.dispatch("notification/setNotice", {});
+        }, 3000);
+        // 削除フラッシュ表示する（失敗時）
         this.$store.commit("auth/setUser", newUser);
       }
     },
