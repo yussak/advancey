6bd555f27aa4ff9ca7b619f7ea850603aa40543c
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-03-19 09:49:21 +0900

    Communityモデル migration実行

---
+++ api/db/migrate/20220319003153_create_communities.rb
@@ -0,0 +1,8 @@
+class CreateCommunities < ActiveRecord::Migration[6.1]
+  def change
+    create_table :communities do |t|
+      t.string :name, null: false
+      t.timestamps
+    end
+  end
+end
--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_20_192344) do
+ActiveRecord::Schema.define(version: 2022_03_19_003153) do
 
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -48,6 +48,12 @@ ActiveRecord::Schema.define(version: 2022_02_20_192344) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "communities", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "inquiries", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
