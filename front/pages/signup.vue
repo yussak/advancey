938e30bec8e17bdf819ab3e4485870a174b21ee6
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-03-24 18:38:28 +0900

    [feat]新規登録ページにゲストログインボタン追加

--- front/pages/signup.vue
+++ front/pages/signup.vue
@@ -1,49 +1,58 @@
 <template>
-  <v-row>
-    <v-col cols="12" md="4">
-      <h2>Sign Up</h2>
-      <form>
-        <v-text-field
-          v-model="name"
-          :counter="10"
-          label="Name"
-          data-vv-name="name"
-          required
-        ></v-text-field>
-        <v-text-field
-          v-model="email"
-          :counter="20"
-          label="Email"
-          data-vv-name="email"
-          required
-        ></v-text-field>
-        <v-text-field
-          v-model="password"
-          label="password"
-          data-vv-name="password"
-          required
-          :type="show1 ? 'text' : 'password'"
-          :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-          @click:append="show1 = !show1"
-        ></v-text-field>
-        <v-text-field
-          v-model="password_confirmation"
-          label="password_confirmation"
-          data-vv-name="password_confirmation"
-          required
-          :type="show2 ? 'text' : 'password'"
-          :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
-          @click:append="show2 = !show2"
-        ></v-text-field>
-        <v-btn class="mr-4" @click="signup">submit</v-btn>
-        <p v-if="error" class="errors">{{ error }}</p>
-      </form>
-    </v-col>
-  </v-row>
+  <div>
+    <v-row>
+      <v-col cols="12" md="4">
+        <h2>Sign Up</h2>
+        <form>
+          <v-text-field
+            v-model="name"
+            :counter="10"
+            label="Name"
+            data-vv-name="name"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="email"
+            :counter="20"
+            label="Email"
+            data-vv-name="email"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="password"
+            label="password"
+            data-vv-name="password"
+            required
+            :type="show1 ? 'text' : 'password'"
+            :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+            @click:append="show1 = !show1"
+          ></v-text-field>
+          <v-text-field
+            v-model="password_confirmation"
+            label="password_confirmation"
+            data-vv-name="password_confirmation"
+            required
+            :type="show2 ? 'text' : 'password'"
+            :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
+            @click:append="show2 = !show2"
+          ></v-text-field>
+          <v-btn class="mr-4" @click="signup">submit</v-btn>
+          <p v-if="error" class="errors">{{ error }}</p>
+        </form>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <p><GuestLoginButton /></p>
+        <p>1クリックでログインできます</p>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 <script>
 import axios from "@/plugins/axios";
 import firebase from "@/plugins/firebase";
+import GuestLoginButton from "@/components/GuestLoginButton";
 
 export default {
   data() {
@@ -56,6 +65,9 @@ export default {
       show2: false,
       error: "",
     };
+  },
+  components: {
+    GuestLoginButton,
   },
   methods: {
     async signup() {
