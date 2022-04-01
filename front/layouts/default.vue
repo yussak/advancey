43d7bfd3109eb33954f4a00ce1f4ad0dbc56e3d9
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-04-01 16:44:58 +0900

    [style]レイアウトをサイドバー＋ヘッダーに変更、リンクを配列で書き換え

--- front/layouts/default.vue
+++ front/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app dark>
+  <v-app>
     <div>
       <Success />
       <Loading />
@@ -21,194 +21,134 @@
       </v-list>
     </v-navigation-drawer> -->
 
-    <!-- 左 -->
-    <div>
-      <v-card>
-        <v-navigation-drawer fixed left permanent class="sidebar">
-          <template v-slot:prepend>
-            <v-list-item two-line :to="`/`">
-              <v-list-item-content>
-                <v-list-item-title class="logo">Advancey</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </template>
-
-          <v-divider></v-divider>
-
-          <v-list dense>
-            <!-- data()の方に書ければシンプルになるかも -->
-            <v-list-item :to="`/`" v-if="user">
-              <v-list-item-content>
-                <v-list-item-title>ホーム</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-            <v-list-item :to="`/users/${this.user.id}`" v-if="user">
-              <v-list-item-content>
-                <v-list-item-title>マイページ</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-            <v-list-item :to="`/topics`">
-              <v-list-item-content>
-                <v-list-item-title>掲示板</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-            <v-list-item :to="`/communities`">
-              <v-list-item-content>
-                <v-list-item-title>コミュニティ</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-            <v-list-item :to="`/about`">
-              <v-list-item-content>
-                <v-list-item-title>サービス詳細</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-            <v-list-item :to="`/users`">
-              <v-list-item-content>
-                <v-list-item-title>ユーザー一覧</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-            <v-list-item :to="`/private_posts`" v-if="user">
-              <v-list-item-content>
-                <v-list-item-title>非公開メモ一覧</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-            <v-list-item v-if="user">
-              <v-btn @click="logOut">ログアウト</v-btn>
-            </v-list-item>
-            <v-list-item :to="`/login`" v-if="!user">
-              <v-list-item-content>
-                <v-list-item-title>ログイン</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-            <v-list-item :to="`/signup`" v-if="!user">
-              <v-list-item-content>
-                <v-list-item-title>新規登録</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-            <v-list-item v-if="!user">
-              <v-list-item-content>
-                <GuestLoginButton />
-              </v-list-item-content>
-            </v-list-item>
-          </v-list>
-        </v-navigation-drawer>
-      </v-card>
-    </div>
-
-    <!-- 右 -->
-    <div>
-      <v-card>
-        <v-navigation-drawer fixed right permanent class="sidebar">
-          <template v-slot:prepend>
-            <v-list-item two-line :to="`/users/${user.id}`" v-if="user">
-              <v-list-item-avatar>
-                <!-- アイコン表示されない→編集でVuex更新してないからかも -->
-                <img
-                  v-if="user.image_url"
-                  :src="user.image_url"
-                  alt="ユーザーアイコン"
-                />
-                <img
-                  v-else
-                  src="~assets/default-user-icon.png"
-                  alt="ユーザーアイコン"
-                />
-                <!-- コミュニティでmessage.user_idとしたときと違う値が出る… -->
-                <!-- {{ user.id }} -->
-              </v-list-item-avatar>
-              <v-list-item-content>
-                <v-list-item-title>{{ user.name }}さん</v-list-item-title>
-                <v-list-item-subtitle>としてログイン中</v-list-item-subtitle>
-              </v-list-item-content>
-            </v-list-item>
-          </template>
-
-          <v-divider></v-divider>
-
-          <v-list dense>
-            <v-list-item>
-              <v-list-item-content>
-                <v-list-item-title>サイドバー</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-            <v-list-item>
-              <v-btn :to="`/`" v-if="user">投稿する</v-btn>
-            </v-list-item>
-            <v-list-item>
-              <v-btn :to="`/topics`" v-if="user">質問する</v-btn>
-            </v-list-item>
-
-            <v-list-item v-if="user">
-              <DeleteUserDialog />
-            </v-list-item>
-
-            <v-list-item v-if="user">
-              <v-dialog v-model="adminDialog">
-                <template v-slot:activator="{ on, attrs }">
-                  <v-btn
-                    color="orange"
-                    dark
-                    v-bind="attrs"
-                    v-on="on"
-                    @click="adminDialog = true"
-                  >
-                    管理者モード
-                  </v-btn>
-                </template>
-                <!-- ダイアログ中身 -->
-                <v-card>
-                  <v-card-title>管理者モード</v-card-title>
-                  <v-card-actions>
+    <!-- スマホ時だけドロワーメニュー追加したい -->
+    <!-- ヘッダー -->
+    <v-app-bar app>
+      <div class="header_wrapper">
+        <div class="header_left">
+          <nuxt-link :to="`/`" class="logo" v-if="user"> Advancey </nuxt-link>
+          <div class="logo" v-else>Advancey</div>
+        </div>
+        <div class="header_right">
+          <div style="display: flex" v-if="user">
+            <v-avatar>
+              <!-- アイコン表示されない→編集でVuex更新してないからかも -->
+              <img
+                v-if="user.image_url"
+                :src="user.image_url"
+                alt="ユーザーアイコン"
+              />
+              <img
+                v-else
+                src="~assets/default-user-icon.png"
+                alt="ユーザーアイコン"
+              />
+              <!-- コミュニティでmessage.user_idとしたときと違う値が出る… -->
+              <!-- {{ user.id }} -->
+            </v-avatar>
+            <p class="bold-text">
+              {{ user.name }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </v-app-bar>
+    <v-main>
+      <v-container class="main">
+        <v-row>
+          <!-- サイドバー -->
+          <v-col cols="3">
+            <v-list dense>
+              <v-list-item
+                v-for="(item, i) in items"
+                :key="i"
+                :to="item.to"
+                router
+                exact
+              >
+                <v-list-item-content>
+                  <v-list-item-title v-text="item.title" />
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item v-if="!user">
+                <v-list-item-content>
+                  <GuestLoginButton />
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item>
+                <v-btn :to="`/`" v-if="user">投稿する</v-btn>
+              </v-list-item>
+              <v-list-item>
+                <v-btn :to="`/topics`" v-if="user">質問する</v-btn>
+              </v-list-item>
+              <v-list-item v-if="user">
+                <DeleteUserDialog />
+              </v-list-item>
+              <v-list-item v-if="user">
+                <v-dialog v-model="adminDialog">
+                  <template v-slot:activator="{ on, attrs }">
                     <v-btn
-                      color="blue darken-1"
-                      text
-                      @click="adminDialog = false"
+                      color="orange"
+                      dark
+                      v-bind="attrs"
+                      v-on="on"
+                      @click="adminDialog = true"
                     >
-                      キャンセル
+                      管理者モード
                     </v-btn>
-                  </v-card-actions>
-                  <v-card-text style="height: 300px">
-                    <form>
-                      <v-text-field
-                        v-model="profile"
-                        label="profile"
-                        required
-                      ></v-text-field>
-                      <p v-if="error" class="errors">{{ error }}</p>
-                    </form>
-                  </v-card-text>
-                  <v-card-actions>
-                    <v-btn color="blue darken-1" text @click="changeAdmin">
-                      送信
-                    </v-btn>
-                    <v-btn
-                      color="blue darken-1"
-                      text
-                      @click="adminDialog = false"
-                    >
-                      キャンセル
-                    </v-btn>
-                  </v-card-actions>
-                </v-card>
-              </v-dialog>
-            </v-list-item>
-          </v-list>
-        </v-navigation-drawer>
-      </v-card>
-    </div>
-
-    <div class="main">
-      <!-- スマホ時だけドロワーメニュー追加したい -->
-      <v-main>
-        <v-container>
-          <Nuxt />
-        </v-container>
-      </v-main>
-      <v-footer app>
-        <!-- フッター表示されてない気がする -->
-        <span>&copy; {{ new Date().getFullYear() }}</span>
-      </v-footer>
-    </div>
+                  </template>
+                  <!-- ダイアログ中身 -->
+                  <v-card>
+                    <v-card-title>管理者モード</v-card-title>
+                    <v-card-actions>
+                      <v-btn
+                        color="blue darken-1"
+                        text
+                        @click="adminDialog = false"
+                      >
+                        キャンセル
+                      </v-btn>
+                    </v-card-actions>
+                    <v-card-text style="height: 300px">
+                      <form>
+                        <v-text-field
+                          v-model="profile"
+                          label="profile"
+                          required
+                        ></v-text-field>
+                        <p v-if="error" class="errors">{{ error }}</p>
+                      </form>
+                    </v-card-text>
+                    <v-card-actions>
+                      <v-btn color="blue darken-1" text @click="changeAdmin">
+                        送信
+                      </v-btn>
+                      <v-btn
+                        color="blue darken-1"
+                        text
+                        @click="adminDialog = false"
+                      >
+                        キャンセル
+                      </v-btn>
+                    </v-card-actions>
+                  </v-card>
+                </v-dialog>
+              </v-list-item>
+              <v-list-item v-if="user">
+                <v-btn @click="logOut">ログアウト</v-btn>
+              </v-list-item>
+            </v-list>
+          </v-col>
+          <!-- メイン -->
+          <v-col cols="9">
+            <Nuxt />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+    <v-footer app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
   </v-app>
 </template>
 
@@ -240,6 +180,51 @@ export default {
   computed: {
     user() {
       return this.$store.state.auth.currentUser;
+    },
+    items() {
+      if (this.user) {
+        return [
+          {
+            title: "ホーム",
+            to: "/",
+          },
+          {
+            title: "マイページ",
+            to: `/users/${this.user.id}`,
+          },
+          {
+            title: "掲示板",
+            to: "/topics",
+          },
+          {
+            title: "コミュニティ",
+            to: "/communities",
+          },
+          {
+            title: "サービス詳細",
+            to: "/about",
+          },
+          {
+            title: "ユーザー一覧",
+            to: "/users",
+          },
+          {
+            title: "非公開メモ",
+            to: "/private_posts",
+          },
+        ];
+      } else {
+        return [
+          {
+            title: "ログイン",
+            to: "/login",
+          },
+          {
+            title: "新規登録",
+            to: "/signup",
+          },
+        ];
+      }
     },
   },
   methods: {
@@ -298,23 +283,10 @@ export default {
   font-size: 30px !important;
   text-decoration: none !important;
 }
-.main {
-  width: 70%;
-  max-width: 1200px;
-  margin: 0 auto;
-}
-.sidebar {
-  width: 15% !important;
-}
-header {
-  width: 70%;
-  margin: 0 auto;
-}
 .errors {
   color: red;
   margin-top: 20px;
 }
-
 /* カレントページでも色を変えない */
 .theme--light.v-btn--active:hover::before,
 .theme--light.v-btn--active::before {
@@ -323,5 +295,20 @@ header {
 .theme--light.v-list-item--active:hover::before,
 .theme--light.v-list-item--active::before {
   background: none !important;
+}
+.main {
+  /* 幅は仮 */
+  max-width: 1200px !important;
+}
+.header_wrapper {
+  display: flex;
+  justify-content: space-between;
+  max-width: 1500px;
+  width: 90%;
+  margin: 0 auto;
+  /* background: orange; */
+}
+.bold-text {
+  font-weight: bold;
 }
 </style>
