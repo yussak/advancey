e57344eedd63c19984888d8745b07df06a0a99d7
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-04-01 17:09:01 +0900

    [refactor]サイドバーをコンポーネント化

---
+++ front/components/SideBar.vue
@@ -0,0 +1,106 @@
+<template>
+  <div>
+    <v-list dense>
+      <v-list-item
+        v-for="(item, i) in items"
+        :key="i"
+        :to="item.to"
+        router
+        exact
+      >
+        <v-list-item-content>
+          <v-list-item-title v-text="item.title" />
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item v-if="!user">
+        <v-list-item-content>
+          <GuestLoginButton />
+        </v-list-item-content>
+      </v-list-item>
+      <v-list-item>
+        <v-btn :to="`/`" v-if="user">投稿する</v-btn>
+      </v-list-item>
+      <v-list-item>
+        <v-btn :to="`/topics`" v-if="user">質問する</v-btn>
+      </v-list-item>
+      <v-list-item v-if="user">
+        <DeleteUserDialog />
+      </v-list-item>
+      <v-list-item v-if="user">
+        <ChangeAdminDialog />
+      </v-list-item>
+      <v-list-item v-if="user">
+        <v-btn @click="logOut">ログアウト</v-btn>
+      </v-list-item>
+    </v-list>
+  </div>
+</template>
+
+<script>
+import firebase from "@/plugins/firebase";
+
+export default {
+  computed: {
+    user() {
+      return this.$store.state.auth.currentUser;
+    },
+    items() {
+      if (this.user) {
+        return [
+          {
+            title: "ホーム",
+            to: "/",
+          },
+          {
+            title: "マイページ",
+            to: `/users/${this.user.id}`,
+          },
+          {
+            title: "掲示板",
+            to: "/topics",
+          },
+          {
+            title: "コミュニティ",
+            to: "/communities",
+          },
+          {
+            title: "サービス詳細",
+            to: "/about",
+          },
+          {
+            title: "ユーザー一覧",
+            to: "/users",
+          },
+          {
+            title: "非公開メモ",
+            to: "/private_posts",
+          },
+        ];
+      } else {
+        return [
+          {
+            title: "ログイン",
+            to: "/login",
+          },
+          {
+            title: "新規登録",
+            to: "/signup",
+          },
+        ];
+      }
+    },
+  },
+  methods: {
+    async logOut() {
+      await firebase
+        .auth()
+        .signOut()
+        .catch((err) => {
+          console.log(err);
+        });
+      this.$store.dispatch("auth/setUser", null);
+      this.$router.push("/login");
+    },
+  },
+};
+</script>
--- front/layouts/default.vue
+++ front/layouts/default.vue
@@ -58,39 +58,7 @@
         <v-row>
           <!-- サイドバー -->
           <v-col cols="3">
-            <v-list dense>
-              <v-list-item
-                v-for="(item, i) in items"
-                :key="i"
-                :to="item.to"
-                router
-                exact
-              >
-                <v-list-item-content>
-                  <v-list-item-title v-text="item.title" />
-                </v-list-item-content>
-              </v-list-item>
-              <v-list-item v-if="!user">
-                <v-list-item-content>
-                  <GuestLoginButton />
-                </v-list-item-content>
-              </v-list-item>
-              <v-list-item>
-                <v-btn :to="`/`" v-if="user">投稿する</v-btn>
-              </v-list-item>
-              <v-list-item>
-                <v-btn :to="`/topics`" v-if="user">質問する</v-btn>
-              </v-list-item>
-              <v-list-item v-if="user">
-                <DeleteUserDialog />
-              </v-list-item>
-              <v-list-item v-if="user">
-                <ChangeAdminDialog />
-              </v-list-item>
-              <v-list-item v-if="user">
-                <v-btn @click="logOut">ログアウト</v-btn>
-              </v-list-item>
-            </v-list>
+            <SideBar />
           </v-col>
           <!-- メイン -->
           <v-col cols="9">
@@ -108,10 +76,10 @@
 <script>
 import Loading from "@/components/Loading";
 import Success from "@/components/Success";
-import firebase from "@/plugins/firebase";
 import GuestLoginButton from "@/components/GuestLoginButton";
 import DeleteUserDialog from "@/components/DeleteUserDialog";
 import ChangeAdminDialog from "@/components/ChangeAdminDialog";
+import SideBar from "@/components/SideBar";
 
 export default {
   data() {
@@ -127,67 +95,11 @@ export default {
     GuestLoginButton,
     DeleteUserDialog,
     ChangeAdminDialog,
+    SideBar,
   },
   computed: {
     user() {
       return this.$store.state.auth.currentUser;
-    },
-    items() {
-      if (this.user) {
-        return [
-          {
-            title: "ホーム",
-            to: "/",
-          },
-          {
-            title: "マイページ",
-            to: `/users/${this.user.id}`,
-          },
-          {
-            title: "掲示板",
-            to: "/topics",
-          },
-          {
-            title: "コミュニティ",
-            to: "/communities",
-          },
-          {
-            title: "サービス詳細",
-            to: "/about",
-          },
-          {
-            title: "ユーザー一覧",
-            to: "/users",
-          },
-          {
-            title: "非公開メモ",
-            to: "/private_posts",
-          },
-        ];
-      } else {
-        return [
-          {
-            title: "ログイン",
-            to: "/login",
-          },
-          {
-            title: "新規登録",
-            to: "/signup",
-          },
-        ];
-      }
-    },
-  },
-  methods: {
-    async logOut() {
-      await firebase
-        .auth()
-        .signOut()
-        .catch((err) => {
-          console.log(err);
-        });
-      this.$store.dispatch("auth/setUser", null);
-      this.$router.push("/login");
     },
   },
 };
