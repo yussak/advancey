36a44369f86bc14912d3823289eaf531872f1ffb
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-04-01 17:32:21 +0900

    [refactor]ユーザー編集ダイアログをコンポーネント化

---
+++ front/components/EditUserDialog.vue
@@ -0,0 +1,150 @@
+<template>
+  <div>
+    <v-row v-if="$store.state.auth.currentUser.id === user.id" justify="center">
+      <v-dialog v-model="editUserDialog" scrollable fullscreen hide-overlay>
+        <!-- ダイアログボタン -->
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            color="primary"
+            dark
+            v-bind="attrs"
+            v-on="on"
+            @click="openEditUserInfoDialog()"
+          >
+            ユーザー情報を編集
+          </v-btn>
+        </template>
+        <!-- ダイアログ中身 -->
+        <v-card>
+          <v-card-title>ユーザー情報編集</v-card-title>
+          <v-card-actions>
+            <v-btn color="blue darken-1" text @click="editUserDialog = false">
+              戻る
+            </v-btn>
+            <v-btn
+              color="blue darken-1"
+              text
+              @click="(editUserDialog = false), updateUserInfo()"
+            >
+              保存する
+            </v-btn>
+          </v-card-actions>
+          <v-card-text style="height: 300px">
+            <v-avatar>
+              <img
+                v-if="user.image_url"
+                :src="user.image_url"
+                alt="ユーザーアイコン"
+              />
+              <img
+                v-else
+                src="~assets/default-user-icon.png"
+                alt="ユーザーアイコン"
+              />
+            </v-avatar>
+            <span style="font-weight: bold">{{ user.name }}さん</span>
+            <form>
+              <v-text-field
+                v-model="name"
+                :counter="10"
+                label="Name"
+                data-vv-name="name"
+                required
+              ></v-text-field>
+              <v-text-field
+                v-model="profile"
+                :counter="50"
+                label="自己紹介を入力してみましょう！"
+                data-vv-name="profile"
+              ></v-text-field>
+              <v-row>
+                <v-col cols="6" md="8">
+                  <v-file-input
+                    v-model="image"
+                    accept="image/*"
+                    label="アイコン画像を変更"
+                    prepend-icon="mdi-camera"
+                    @change="setImage"
+                  >
+                  </v-file-input>
+                </v-col>
+              </v-row>
+            </form>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="blue darken-1" text @click="editUserDialog = false">
+              戻る
+            </v-btn>
+            <v-btn
+              color="blue darken-1"
+              text
+              @click="(editUserDialog = false), updateUserInfo()"
+            >
+              保存する
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import axios from "@/plugins/axios";
+
+export default {
+  data() {
+    return {
+      editUserDialog: false,
+      name: "",
+      profile: "",
+      imageFile: null,
+      image: [],
+    };
+  },
+  computed: {
+    user() {
+      return this.$store.state.auth.currentUser;
+    },
+  },
+  methods: {
+    setImage(e) {
+      this.imageFile = e;
+    },
+    openEditUserInfoDialog() {
+      this.name = this.user.name;
+      this.profile = this.user.profile;
+      this.image = this.user.image;
+    },
+    updateUserInfo() {
+      const config = {
+        headers: {
+          "content-type": "multipart/form-data",
+        },
+      };
+      let formData = new FormData();
+      formData.append("user[name]", this.name);
+      formData.append("user[profile]", this.profile);
+      if (this.imageFile !== null) {
+        formData.append("user[image]]", this.imageFile);
+      }
+      axios
+        .put(`/v1/users/${this.$route.params.id}`, formData, config)
+        .then(() => {
+          // this.fetchUserInfo();
+          // Vuexを更新したい
+          this.$store.dispatch("notification/setNotice", {
+            status: true,
+            message: "ユーザーを編集しました",
+          });
+          setTimeout(() => {
+            this.$store.dispatch("notification/setNotice", {});
+          }, 3000);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+};
+</script>
--- front/pages/users/_id.vue
+++ front/pages/users/_id.vue
@@ -11,7 +11,6 @@
     <p v-if="user.profile">自己紹介：{{ user.profile }}</p>
     <p v-else>自己紹介：よろしくおねがいします！</p>
 
-    <!-- ユーザー編集ダイアログ -->
     <!-- コンポーネントにしたい -->
     <!-- 自分のページ以外で表示する -->
     <!-- たまに動かない（要修正）けど基本これでフォロー・解除出来る -->
@@ -126,92 +125,8 @@
     </v-row>
 
     <!-- 自分のページの時だけ表示する -->
-    <v-row v-if="$store.state.auth.currentUser.id === user.id" justify="center">
-      <v-dialog v-model="user_info_dialog" scrollable fullscreen hide-overlay>
-        <!-- ダイアログボタン -->
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            color="primary"
-            dark
-            v-bind="attrs"
-            v-on="on"
-            @click="openEditUserInfoDialog()"
-          >
-            ユーザー情報を編集
-          </v-btn>
-        </template>
-        <!-- ダイアログ中身 -->
-        <v-card>
-          <v-card-title>ユーザー情報編集</v-card-title>
-          <v-card-actions>
-            <v-btn color="blue darken-1" text @click="user_info_dialog = false">
-              戻る
-            </v-btn>
-            <v-btn
-              color="blue darken-1"
-              text
-              @click="(user_info_dialog = false), updateUserInfo()"
-            >
-              保存する
-            </v-btn>
-          </v-card-actions>
-          <v-card-text style="height: 300px">
-            <v-avatar>
-              <img
-                v-if="user.image_url"
-                :src="user.image_url"
-                alt="ユーザーアイコン"
-              />
-              <img
-                v-else
-                src="~assets/default-user-icon.png"
-                alt="ユーザーアイコン"
-              />
-            </v-avatar>
-            <span style="font-weight: bold">{{ user.name }}さん</span>
-            <form>
-              <v-text-field
-                v-model="name"
-                :counter="10"
-                label="Name"
-                data-vv-name="name"
-                required
-              ></v-text-field>
-              <v-text-field
-                v-model="profile"
-                :counter="50"
-                label="自己紹介を入力してみましょう！"
-                data-vv-name="profile"
-              ></v-text-field>
-              <v-row>
-                <v-col cols="6" md="8">
-                  <v-file-input
-                    v-model="image"
-                    accept="image/*"
-                    label="アイコン画像を変更"
-                    prepend-icon="mdi-camera"
-                    @change="setImage"
-                  >
-                  </v-file-input>
-                </v-col>
-              </v-row>
-            </form>
-          </v-card-text>
-          <v-card-actions>
-            <v-btn color="blue darken-1" text @click="user_info_dialog = false">
-              戻る
-            </v-btn>
-            <v-btn
-              color="blue darken-1"
-              text
-              @click="(user_info_dialog = false), updateUserInfo()"
-            >
-              保存する
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-    </v-row>
+    <!-- ユーザー編集ダイアログ -->
+    <EditUserDialog />
 
     <v-row>
       <v-col>
@@ -224,6 +139,7 @@
 
 <script>
 import PostList from "@/components/PostList";
+import EditUserDialog from "@/components/EditUserDialog";
 import axios from "@/plugins/axios";
 
 export default {
@@ -235,15 +151,16 @@ export default {
   },
   components: {
     PostList,
+    EditUserDialog,
   },
   data() {
     return {
       user: [],
       posts: [],
-      user_info_dialog: false,
+      // user_info_dialog: false,
       user_follow_dialog: false,
-      name: "",
-      profile: "",
+      // name: "",
+      // profile: "",
       followers: [],
       following: [],
       isFollowed: false,
@@ -266,9 +183,9 @@ export default {
     },
   },
   methods: {
-    setImage(e) {
-      this.imageFile = e;
-    },
+    // setImage(e) {
+    //   this.imageFile = e;
+    // },
     getFollowers() {
       axios.get(`/v1/users/${this.$route.params.id}/followers`).then((res) => {
         this.followers = res.data;
@@ -335,39 +252,39 @@ export default {
     },
     // @click=dialog name=trueで書き換えればこれ消せる（default.vue参考）
     openUserFollowerListDialog() {},
-    openEditUserInfoDialog() {
-      this.name = this.user.name;
-      this.profile = this.user.profile;
-      this.image = this.user.image;
-    },
-    updateUserInfo() {
-      const config = {
-        headers: {
-          "content-type": "multipart/form-data",
-        },
-      };
-      let formData = new FormData();
-      formData.append("user[name]", this.name);
-      formData.append("user[profile]", this.profile);
-      if (this.imageFile !== null) {
-        formData.append("user[image]]", this.imageFile);
-      }
-      axios
-        .put(`/v1/users/${this.$route.params.id}`, formData, config)
-        .then(() => {
-          this.fetchUserInfo();
-          this.$store.dispatch("notification/setNotice", {
-            status: true,
-            message: "ユーザーを編集しました",
-          });
-          setTimeout(() => {
-            this.$store.dispatch("notification/setNotice", {});
-          }, 3000);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    },
+    // openEditUserInfoDialog() {
+    //   this.name = this.user.name;
+    //   this.profile = this.user.profile;
+    //   this.image = this.user.image;
+    // },
+    // updateUserInfo() {
+    //   const config = {
+    //     headers: {
+    //       "content-type": "multipart/form-data",
+    //     },
+    //   };
+    //   let formData = new FormData();
+    //   formData.append("user[name]", this.name);
+    //   formData.append("user[profile]", this.profile);
+    //   if (this.imageFile !== null) {
+    //     formData.append("user[image]]", this.imageFile);
+    //   }
+    //   axios
+    //     .put(`/v1/users/${this.$route.params.id}`, formData, config)
+    //     .then(() => {
+    //       this.fetchUserInfo();
+    //       this.$store.dispatch("notification/setNotice", {
+    //         status: true,
+    //         message: "ユーザーを編集しました",
+    //       });
+    //       setTimeout(() => {
+    //         this.$store.dispatch("notification/setNotice", {});
+    //       }, 3000);
+    //     })
+    //     .catch((err) => {
+    //       console.log(err);
+    //     });
+    // },
   },
 };
 </script>
