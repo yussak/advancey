bc4bd540025352c7c3b7e1108e77d2892cfe10fe
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-04-03 10:58:42 +0900

    [feat]目標コメント、モックアプ作成（日付毎）

--- front/pages/goals/_id.vue
+++ front/pages/goals/_id.vue
@@ -29,10 +29,12 @@
             ref="calendar"
             v-model="focus"
             color="primary"
+            type="month"
             :events="events"
             @click:event="showEvent"
-            @change="updateRange"
           ></v-calendar>
+          <!-- @change="updateRange" -->
+
           <v-menu
             v-model="goalTodoComment"
             :close-on-content-click="false"
@@ -41,6 +43,9 @@
           >
             <v-card color="grey lighten-4" min-width="350px" flat>
               <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
+              <v-card-text>
+                <!-- <span v-html="selectedEvent.detail"></span> -->
+              </v-card-text>
               <v-card-actions>
                 <v-btn text @click="goalTodoComment = false">Cancel</v-btn>
               </v-card-actions>
@@ -68,14 +73,28 @@ export default {
       selectedEvent: [], //これをcommentにするかも
       selectedElement: null,
       goalTodoComment: false,
-      events: [],
-      names: [
-        "８セットやりきった",
-        "7セット 腕立ては深くなくていいかも",
-        "6セット １セット５回出来た",
-        "時間無かったが４セットだけやった",
-        "サボりそうだったけど８セット完走！！",
-        "５セット",
+      // events: [],
+      events: [
+        {
+          name: "てｓｔ",
+          start: "2022-3-1",
+          end: "2022-3-5",
+        },
+        {
+          name: "５セットやった",
+          start: "2022-4-1",
+          end: "2022-4-1",
+        },
+        {
+          name: "疲れたけど3セットだけやった",
+          start: "2022-4-2",
+          end: "2022-4-2",
+        },
+        {
+          name: "8セット完走。腕立ては浅くていいかも",
+          start: "2022-4-3",
+          end: "2022-4-3",
+        },
       ],
     };
   },
@@ -134,34 +153,34 @@ export default {
       }
       nativeEvent.stopPropagation();
     },
-    updateRange({ start, end }) {
-      const events = [];
+    //   updateRange({ start, end }) {
+    //     const events = [];
 
-      const min = new Date(`${start.date}T00:00:00`);
-      const max = new Date(`${end.date}T23:59:59`);
-      const days = (max.getTime() - min.getTime()) / 86400000;
-      const eventCount = this.rnd(days, days + 20);
+    //     const min = new Date(`${start.date}T00:00:00`);
+    //     const max = new Date(`${end.date}T23:59:59`);
+    //     const days = (max.getTime() - min.getTime()) / 86400000;
+    //     const eventCount = this.rnd(days, days + 20);
 
-      for (let i = 0; i < eventCount; i++) {
-        const allDay = this.rnd(0, 3) === 0;
-        const firstTimestamp = this.rnd(min.getTime(), max.getTime());
-        const first = new Date(firstTimestamp - (firstTimestamp % 900000));
-        const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000;
-        const second = new Date(first.getTime() + secondTimestamp);
+    //     for (let i = 0; i < eventCount; i++) {
+    //       const allDay = this.rnd(0, 3) === 0;
+    //       const firstTimestamp = this.rnd(min.getTime(), max.getTime());
+    //       const first = new Date(firstTimestamp - (firstTimestamp % 900000));
+    //       const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000;
+    //       const second = new Date(first.getTime() + secondTimestamp);
 
-        events.push({
-          name: this.names[this.rnd(0, this.names.length - 1)],
-          start: first,
-          end: second,
-          timed: !allDay,
-        });
-      }
+    //       events.push({
+    //         name: this.names[this.rnd(0, this.names.length - 1)],
+    //         start: first,
+    //         end: second,
+    //         timed: !allDay,
+    //       });
+    //     }
 
-      this.events = events;
-    },
-    rnd(a, b) {
-      return Math.floor((b - a + 1) * Math.random()) + a;
-    },
+    //     this.events = events;
+    //   },
+    //   rnd(a, b) {
+    //     return Math.floor((b - a + 1) * Math.random()) + a;
+    //   },
   },
 };
 </script>
