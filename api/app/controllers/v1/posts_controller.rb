21fd8eeb91f200d42ec981ce4b1d0eb00ea9adcb
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-04-26 23:42:04 +0900

    [fix]ユーザーアイコンが表示されないの修正

--- api/app/controllers/v1/posts_controller.rb
+++ api/app/controllers/v1/posts_controller.rb
@@ -22,7 +22,7 @@ class V1::PostsController < ApplicationController
   def show
     post = Post.find(params[:id])
     render json: post.to_json(methods: [:image_url],
-                              include: [{ user: { only: :name } },
+                              include: [{ user: { methods: :image_url, only: :name } },
                                         { post_comments: { except: :updated_at,
                                                            methods: :image_url, include: { user: { only: :name, methods: :image_url } } } }])
   end
