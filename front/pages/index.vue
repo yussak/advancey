34ffe98c032939e57b77bb6fdbc8c18447d84b27
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-05-01 05:53:06 +0900

    [feat]Top　タブ原形作成

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,8 +1,131 @@
 <template>
   <div v-if="user">
-    <UserCard :user="user" />
-    <PostForm @submit="addPost" />
+    <!-- <UserCard :user="user" /> -->
+    <!-- <PostForm @submit="addPost" /> -->
     <!-- <PostList :posts="posts" /> -->
+    <v-card>
+      <v-tabs grow>
+        <v-tab v-for="title in outerTitles" :key="title.id">
+          {{ title.name }}
+        </v-tab>
+        <!-- メモ -->
+        <v-tab-item>
+          <v-row dense>
+            <v-col>
+              <v-card>
+                <v-tabs grow>
+                  <v-tab v-for="title in memoTitles" :key="title.id">
+                    {{ title.name }}
+                  </v-tab>
+                  <v-tab-item>
+                    <v-row dense>
+                      <v-col>memo1</v-col>
+                    </v-row>
+                  </v-tab-item>
+                  <v-tab-item>
+                    <v-row dense>
+                      <v-col>memo2</v-col>
+                    </v-row>
+                  </v-tab-item>
+                  <v-tab-item>
+                    <v-row dense>
+                      <v-col>memo3</v-col>
+                    </v-row>
+                  </v-tab-item>
+                  <v-tab-item>
+                    <v-row dense>
+                      <v-col>memo4</v-col>
+                    </v-row>
+                  </v-tab-item>
+                </v-tabs>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-tab-item>
+        <!-- 質問 -->
+        <v-tab-item>
+          <v-row dense>
+            <v-col>
+              <v-card>
+                <v-tabs grow>
+                  <v-tab v-for="title in topicTitles" :key="title.id">
+                    {{ title.name }}
+                  </v-tab>
+                  <v-tab-item>
+                    <v-row dense>
+                      <v-col>memo1</v-col>
+                    </v-row>
+                  </v-tab-item>
+                  <v-tab-item>
+                    <v-row dense>
+                      <v-col>memo2</v-col>
+                    </v-row>
+                  </v-tab-item>
+                  <v-tab-item>
+                    <v-row dense>
+                      <v-col>memo3</v-col>
+                    </v-row>
+                  </v-tab-item>
+                  <v-tab-item>
+                    <v-row dense>
+                      <v-col>memo4</v-col>
+                    </v-row>
+                  </v-tab-item>
+                </v-tabs>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-tab-item>
+        <!-- 目標 -->
+        <v-tab-item>
+          <v-row dense>
+            <v-col>
+              <v-card>
+                <v-tabs grow>
+                  <v-tab v-for="title in goalTitles" :key="title.id">
+                    {{ title.name }}
+                  </v-tab>
+                  <v-tab-item>
+                    <v-row dense>
+                      <v-col>goal1</v-col>
+                    </v-row>
+                  </v-tab-item>
+                  <v-tab-item>
+                    <v-row dense>
+                      <v-col>goal2</v-col>
+                    </v-row>
+                  </v-tab-item>
+                  <v-tab-item>
+                    <v-row dense>
+                      <v-col>goal3</v-col>
+                    </v-row>
+                  </v-tab-item>
+                </v-tabs>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-tab-item>
+        <!-- コミュニティ -->
+        <v-tab-item>
+          <v-row dense>
+            <v-col>
+              <v-card>
+                <v-tabs grow>
+                  <v-tab v-for="title in communityTitles" :key="title.id">
+                    {{ title.name }}
+                  </v-tab>
+                  <v-tab-item>
+                    <v-row dense>
+                      <v-col>community1</v-col>
+                    </v-row>
+                  </v-tab-item>
+                </v-tabs>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-tab-item>
+      </v-tabs>
+    </v-card>
   </div>
 </template>
 
@@ -25,6 +148,21 @@ export default {
   data() {
     return {
       // posts: [],
+      outerTitles: [
+        { name: "メモ" },
+        { name: "Q&A" },
+        { name: "目標" },
+        { name: "コミュニティ" },
+      ],
+      memoTitles: [
+        { name: "全て" },
+        { name: "実践中" },
+        { name: "実践したい" },
+        { name: "身についた" },
+      ],
+      topicTitles: [{ name: "全て" }, { name: "未解決" }, { name: "解決済み" }],
+      goalTitles: [{ name: "全て" }, { name: "未達成" }, { name: "達成済み" }],
+      communityTitles: [{ name: "全て" }],
     };
   },
   computed: {
