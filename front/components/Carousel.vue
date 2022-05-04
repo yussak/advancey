7ebd1ca49603d237f24abfa6f9b58fea287907b3
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-05-05 08:11:44 +0900

    [style]カルーセル調整

--- front/components/Carousel.vue
+++ front/components/Carousel.vue
@@ -1,66 +1,66 @@
 <template>
   <div>
-    <v-carousel
-      cycle
-      height="400"
-      hide-delimiter-background
-      show-arrows-on-hover
-    >
+    <!-- cycle -->
+    <v-carousel height="400" hide-delimiter-background show-arrows-on-hover>
       <v-carousel-item>
-        <v-sheet color="light-green lighten-3" height="100%">
+        <v-sheet
+          color="light-green lighten-3"
+          height="100%"
+          style="padding: 0 30px"
+        >
           <v-row class="fill-height" align="center" justify="center">
             <div class="text-center">
-              <p class="text-h2">Advanceyの使い方</p>
+              <p class="text-h3">Advanceyの使い方</p>
             </div>
           </v-row>
         </v-sheet>
       </v-carousel-item>
       <v-carousel-item>
-        <v-sheet color="pink lighten-4" height="100%">
+        <v-sheet color="pink lighten-4" height="100%" style="padding: 0 30px">
           <v-row class="fill-height" align="center" justify="center">
             <div class="text-center">
-              <p class="text-h2">メモを追加しましょう</p>
-              <p class="text-h4">役立ったことなど何でもメモしてみましょう</p>
-              <p class="text-h4">
+              <p class="text-h4">メモを追加しましょう</p>
+              <p class="text-h5">役立ったことなど何でもメモしてみましょう</p>
+              <p class="text-h5">
                 やっていること・やりたいこと・やってよかったことに<br />分類できます
               </p>
-              <p class="text-h4">非公開にすると自分だけが閲覧できます</p>
+              <p class="text-h5">非公開にすると自分だけが閲覧できます</p>
             </div>
           </v-row>
         </v-sheet>
       </v-carousel-item>
       <v-carousel-item>
-        <v-sheet color="blue lighten-4" height="100%">
+        <v-sheet color="blue lighten-4" height="100%" style="padding: 0 30px">
           <v-row class="fill-height" align="center" justify="center">
             <div class="text-center">
-              <p class="text-h2">質問をしてみましょう</p>
-              <p class="text-h4">分かる質問には回答してみましょう</p>
-              <p class="text-h4">解決した問題はクローズすることができます。</p>
+              <p class="text-h4">質問をしてみましょう</p>
+              <p class="text-h5">分かる質問には回答してみましょう</p>
+              <p class="text-h5">解決した問題はクローズすることができます。</p>
             </div>
           </v-row>
         </v-sheet>
       </v-carousel-item>
       <v-carousel-item>
-        <v-sheet color="lime lighten-4" height="100%">
+        <v-sheet color="lime lighten-4" height="100%" style="padding: 0 30px">
           <v-row class="fill-height" align="center" justify="center">
             <div class="text-center">
-              <p class="text-h2">目標を立てましょう</p>
-              <p class="text-h4">
+              <p class="text-h4">目標を立てましょう</p>
+              <p class="text-h5">
                 達成したいこと・その理由・やること<br />を明確にし、継続・達成しましょう
               </p>
-              <p class="text-h4">カレンダーに毎日コメントできます</p>
+              <p class="text-h5">カレンダーに毎日コメントできます</p>
             </div>
           </v-row>
         </v-sheet>
       </v-carousel-item>
       <v-carousel-item>
-        <v-sheet color="orange lighten-4" height="100%">
+        <v-sheet color="orange lighten-4" height="100%" style="padding: 0 30px">
           <v-row class="fill-height" align="center" justify="center">
             <div class="text-center">
-              <p class="text-h2">コミュニティに参加しましょう</p>
-              <p class="text-h4">テーマを決めて仲間と集まりましょう</p>
-              <p class="text-h4">リアルタイムチャットで盛り上がりましょう</p>
-              <p class="text-h4">出入りは自由です</p>
+              <p class="text-h4">コミュニティに参加しましょう</p>
+              <p class="text-h5">テーマを決めて仲間と集まりましょう</p>
+              <p class="text-h5">リアルタイムチャットで盛り上がりましょう</p>
+              <p class="text-h5">出入りは自由です</p>
             </div>
           </v-row>
         </v-sheet>
