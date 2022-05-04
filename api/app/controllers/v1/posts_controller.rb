69b91496703dbf2fd4f959fc52bd8263ee100d56
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-05-04 19:08:58 +0900

    [fix]チャットメッセージUserCard修正

--- api/app/controllers/v1/posts_controller.rb
+++ api/app/controllers/v1/posts_controller.rb
@@ -2,7 +2,7 @@ class V1::PostsController < ApplicationController
   def index
     posts = Post.all
     render json: posts.to_json(methods: :image_url, except: :updated_at,
-                               include: { user: { methods: :image_url, only: :name } })
+                               include: { user: { methods: :image_url, only: %i[id name admin] } })
   end
 
   def create
