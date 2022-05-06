04c5b51c6ee4f3533f35ad088ec4ac54323890da
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-05-07 06:23:01 +0900

    [refactor]TOPタブコンポーネント化

---
+++ front/components/TopTabList.vue
@@ -0,0 +1,411 @@
+<template>
+  <div>
+    <v-card grow>
+      <v-tabs>
+        <!-- メモ、質問、目標、コミュニティ -->
+        <v-tab v-for="title in outerTitles" :key="title.id">
+          {{ title.name }}
+        </v-tab>
+        <!-- メモ -->
+        <v-tab-item>
+          <v-row dense>
+            <v-col>
+              <v-card>
+                <v-tabs grow>
+                  <v-tab v-for="title in memoTitles" :key="title.id">
+                    {{ title.name }}
+                  </v-tab>
+                  <v-tab-item>
+                    <v-row dense>
+                      <v-col v-if="!(posts && posts.length)"
+                        >メモがありません</v-col
+                      >
+                      <v-col
+                        v-else
+                        v-for="post in posts"
+                        :key="post.id"
+                        cols="12"
+                        md="6"
+                      >
+                        <PostCard
+                          v-if="user"
+                          :post="post"
+                          @submitDeletePost="handleSubmitDeletePost"
+                        />
+                        <NotLoginPostCard
+                          v-else
+                          :user="post.user"
+                          :post="post"
+                        />
+                      </v-col>
+                    </v-row>
+                  </v-tab-item>
+                  <v-tab-item>
+                    <v-row dense>
+                      <v-col v-if="!(posts && doing_posts.length)"
+                        >メモがありません</v-col
+                      >
+                      <v-col
+                        v-else
+                        v-for="post in doing_posts"
+                        :key="post.id"
+                        cols="12"
+                        md="6"
+                      >
+                        <PostCard
+                          v-if="user"
+                          :post="post"
+                          @submitDeletePost="handleSubmitDeletePost"
+                        />
+                        <NotLoginPostCard
+                          v-else
+                          :user="post.user"
+                          :post="post"
+                        />
+                      </v-col>
+                    </v-row>
+                  </v-tab-item>
+                  <v-tab-item>
+                    <v-row dense>
+                      <v-col v-if="!(posts && want_posts.length)"
+                        >メモがありません</v-col
+                      >
+                      <v-col
+                        v-else
+                        v-for="post in want_posts"
+                        :key="post.id"
+                        cols="12"
+                        md="6"
+                      >
+                        <PostCard
+                          v-if="user"
+                          :post="post"
+                          @submitDeletePost="handleSubmitDeletePost"
+                        />
+                        <NotLoginPostCard
+                          v-else
+                          :user="post.user"
+                          :post="post"
+                        />
+                      </v-col>
+                    </v-row>
+                  </v-tab-item>
+                  <v-tab-item>
+                    <v-row dense>
+                      <v-col v-if="!(posts && master_posts.length)"
+                        >メモがありません</v-col
+                      >
+                      <v-col
+                        v-else
+                        v-for="post in master_posts"
+                        :key="post.id"
+                        cols="12"
+                        md="6"
+                      >
+                        <PostCard
+                          v-if="user"
+                          :post="post"
+                          @submitDeletePost="handleSubmitDeletePost"
+                        />
+                        <NotLoginPostCard
+                          v-else
+                          :user="post.user"
+                          :post="post"
+                        />
+                      </v-col>
+                    </v-row>
+                  </v-tab-item>
+                </v-tabs>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-tab-item>
+        <!-- 質問 -->
+        <v-tab-item>
+          <v-row dense>
+            <v-col>
+              <v-card>
+                <v-tabs grow>
+                  <v-tab v-for="title in topicTitles" :key="title.id">
+                    {{ title.name }}
+                  </v-tab>
+                  <v-tab-item>
+                    <v-row dense>
+                      <v-col v-if="!(topics && topics.length)"
+                        >質問がありません</v-col
+                      >
+                      <v-col
+                        v-else
+                        v-for="topic in topics"
+                        :key="topic.id"
+                        cols="12"
+                      >
+                        <TopicCard
+                          v-if="user"
+                          :topic="topic"
+                          @submitDeleteTopic="handleSubmitDeleteTopic"
+                        />
+                        <NotLoginTopicCard
+                          v-else
+                          :user="topic.user"
+                          :topic="topic"
+                        />
+                      </v-col>
+                    </v-row>
+                  </v-tab-item>
+                  <v-tab-item>
+                    <v-row dense>
+                      <v-col v-if="!(topics && unsolved_topics.length)"
+                        >質問がありません</v-col
+                      >
+                      <v-col
+                        v-else
+                        v-for="topic in unsolved_topics"
+                        :key="topic.id"
+                        cols="12"
+                      >
+                        <TopicCard
+                          v-if="user"
+                          :topic="topic"
+                          @submitDeleteTopic="handleSubmitDeleteTopic"
+                        />
+                        <NotLoginTopicCard
+                          v-else
+                          :user="topic.user"
+                          :topic="topic"
+                        />
+                      </v-col>
+                    </v-row>
+                  </v-tab-item>
+                  <v-tab-item>
+                    <v-row dense>
+                      <v-col v-if="!(topics && solved_topics.length)"
+                        >質問がありません</v-col
+                      >
+                      <v-col
+                        v-else
+                        v-for="topic in solved_topics"
+                        :key="topic.id"
+                        cols="12"
+                      >
+                        <TopicCard
+                          v-if="user"
+                          :topic="topic"
+                          @submitDeleteTopic="handleSubmitDeleteTopic"
+                        />
+                        <NotLoginTopicCard
+                          v-else
+                          :user="topic.user"
+                          :topic="topic"
+                        />
+                      </v-col>
+                    </v-row>
+                  </v-tab-item>
+                </v-tabs>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-tab-item>
+        <!-- 目標 -->
+        <v-tab-item>
+          <v-row dense>
+            <v-col>
+              <v-card>
+                <v-tabs grow>
+                  <v-tab v-for="title in goalTitles" :key="title.id">
+                    {{ title.name }}
+                  </v-tab>
+                  <v-tab-item>
+                    <v-row dense>
+                      <v-col v-if="!(goals && goals.length)"
+                        >目標がありません</v-col
+                      >
+                      <v-col
+                        v-else
+                        v-for="goal in goals"
+                        :key="goal.id"
+                        cols="12"
+                      >
+                        <GoalCard
+                          v-if="user"
+                          :goal="goal"
+                          @submitDeleteGoal="handleSubmitDeleteGoal"
+                        />
+                        <NotLoginGoalCard
+                          v-else
+                          :user="goal.user"
+                          :goal="goal"
+                        />
+                      </v-col>
+                    </v-row>
+                  </v-tab-item>
+                  <v-tab-item>
+                    <v-row dense>
+                      <v-col v-if="!(goals && unachieved_goals.length)"
+                        >目標がありません</v-col
+                      >
+                      <v-col
+                        v-else
+                        v-for="goal in unachieved_goals"
+                        :key="goal.id"
+                        cols="12"
+                      >
+                        <GoalCard
+                          v-if="user"
+                          :goal="goal"
+                          @submitDeleteGoal="handleSubmitDeleteGoal"
+                        />
+                        <NotLoginGoalCard
+                          v-else
+                          :user="goal.user"
+                          :goal="goal"
+                        />
+                      </v-col>
+                    </v-row>
+                  </v-tab-item>
+                  <v-tab-item>
+                    <v-row dense>
+                      <v-col v-if="!(goals && achieved_goals.length)"
+                        >目標がありません</v-col
+                      >
+                      <v-col
+                        v-else
+                        v-for="goal in achieved_goals"
+                        :key="goal.id"
+                        cols="12"
+                      >
+                        <GoalCard
+                          v-if="user"
+                          :goal="goal"
+                          @submitDeleteGoal="handleSubmitDeleteGoal"
+                        />
+                        <NotLoginGoalCard
+                          v-else
+                          :user="goal.user"
+                          :goal="goal"
+                        />
+                      </v-col>
+                    </v-row>
+                  </v-tab-item>
+                </v-tabs>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-tab-item>
+        <!-- コミュニティ -->
+        <v-tab-item>
+          <v-row dense>
+            <v-col>
+              <v-card>
+                <v-tabs grow>
+                  <v-tab v-for="title in communityTitles" :key="title.id">
+                    {{ title.name }}
+                  </v-tab>
+                  <v-tab-item>
+                    <v-row dense>
+                      <v-col v-if="!(communities && communities.length)"
+                        >コミュニティがありません</v-col
+                      >
+                      <v-col
+                        v-else
+                        v-for="community in communities"
+                        :key="community.id"
+                        cols="12"
+                      >
+                        <CommunityCard
+                          v-if="user"
+                          :community="community"
+                          @submitDeleteCommunity="handleSubmitDeleteCommunity"
+                        />
+                        <NotLoginCommunityCard v-else :community="community" />
+                      </v-col>
+                    </v-row>
+                  </v-tab-item>
+                </v-tabs>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-tab-item>
+      </v-tabs>
+    </v-card>
+  </div>
+</template>
+
+<script>
+import PostCard from "@/components/PostCard";
+import NotLoginPostCard from "@/components/NotLoginPostCard";
+import TopicCard from "@/components/TopicCard";
+import NotLoginTopicCard from "@/components/NotLoginTopicCard";
+import GoalCard from "@/components/GoalCard";
+import NotLoginGoalCard from "@/components/NotLoginGoalCard";
+import CommunityCard from "@/components/CommunityCard";
+import NotLoginCommunityCard from "@/components/NotLoginCommunityCard";
+
+export default {
+  components: {
+    PostCard,
+    NotLoginPostCard,
+    TopicCard,
+    NotLoginTopicCard,
+    GoalCard,
+    NotLoginGoalCard,
+    CommunityCard,
+    NotLoginCommunityCard,
+  },
+  props: [
+    "posts",
+    "doing_posts",
+    "want_posts",
+    "master_posts",
+    "topics",
+    "unsolved_topics",
+    "solved_topics",
+    "goals",
+    "achieved_goals",
+    "unachieved_goals",
+    "communities",
+  ],
+  data() {
+    return {
+      outerTitles: [
+        { name: "メモ" },
+        { name: "Q&A" },
+        { name: "目標" },
+        { name: "コミュニティ" },
+      ],
+      memoTitles: [
+        { name: "新着" },
+        { name: "実践中" },
+        { name: "実践したい" },
+        { name: "やって良かった" },
+      ],
+      topicTitles: [{ name: "新着" }, { name: "受付中" }, { name: "解決済み" }],
+      goalTitles: [{ name: "新着" }, { name: "未達成" }, { name: "達成済み" }],
+      communityTitles: [{ name: "新着" }],
+    };
+  },
+  computed: {
+    user() {
+      return this.$store.state.auth.currentUser;
+    },
+  },
+  methods: {
+    // メモ
+    handleSubmitDeletePost(post) {
+      this.$emit("submitDeletePost", post);
+    },
+    // 質問
+    handleSubmitDeleteTopic(topic) {
+      this.$emit("submitDeleteTopic", topic);
+    },
+    // 目標
+    handleSubmitDeleteGoal(goal) {
+      this.$emit("submitDeleteGoal", goal);
+    },
+    // コミュニティ
+    handleSubmitDeleteCommunity(community) {
+      this.$emit("submitDeleteCommunity", community);
+    },
+  },
+};
+</script>
