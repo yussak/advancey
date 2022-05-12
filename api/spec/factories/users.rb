b17640a9ea75853cf0033427b9863b4b4358e709
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-05-12 19:01:09 +0900

    [test]ユーザー詳細テスト追加

--- api/spec/factories/users.rb
+++ api/spec/factories/users.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
     name { 'user1@aa.com' }
     email { 'user1@aa.com' }
     uid { 'qQ3sMT8AKZZauRjeqhRarLaPFpu1' }
+    admin { false }
   end
 
   factory :other_user, class: User do
@@ -11,5 +12,13 @@ FactoryBot.define do
     name { 'local1@a.com' }
     email { 'local1@a.com' }
     uid { 'gl5JbR9IRKWfCBbCJi8t6r59PMl2' }
+  end
+
+  factory :admin_user, class: User do
+    id { 2 }
+    name { 'admin@a.com' }
+    email { 'local2@a.com' }
+    uid { '4FM141TYlsXszwEbw0JBJO902r32' }
+    admin { true }
   end
 end
--- api/spec/requests/api/v1/users_spec.rb
+++ api/spec/requests/api/v1/users_spec.rb
@@ -13,10 +13,24 @@ RSpec.describe 'Api::V1::Users', type: :request do
       expect((json.length)).to eq(2)
       # ユーザー名が正しいか確認
       expect(json[0]['name']).to eq(user.name)
-      expect(json[0]['zname']).to_not eq(other_user.name)
+      expect(json[0]['name']).to_not eq(other_user.name)
       # ユーザー名が正しくないか確認
       expect(json[1]['name']).to eq(other_user.name)
       expect(json[1]['name']).to_not eq(user.name)
+    end
+  end
+
+  describe 'GET /show' do
+    it 'user詳細ページを表示' do
+      user = FactoryBot.create(:user)
+      get "/v1/users/#{user.id}"
+      json = JSON.parse(response.body)
+
+      # リクエストが成功するか確認
+      expect(response.status).to eq(200)
+      # 値が正しいか確認
+      expect(json['user']['name']).to eq(user.name)
+      expect(json['user']['email']).to eq(user.email)
     end
   end
 end
