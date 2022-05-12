7c17f60cb3a7812d11d59a8055c351a2172fb296
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-05-12 20:17:20 +0900

    [test]ユーザー削除テスト追加

--- api/spec/requests/api/v1/users_spec.rb
+++ api/spec/requests/api/v1/users_spec.rb
@@ -25,7 +25,6 @@ RSpec.describe 'Api::V1::Users', type: :request do
       user = FactoryBot.create(:user)
       get "/v1/users/#{user.id}"
       json = JSON.parse(response.body)
-
       # リクエストが成功するか確認
       expect(response.status).to eq(200)
       # 値が正しいか確認
@@ -37,32 +36,36 @@ RSpec.describe 'Api::V1::Users', type: :request do
   describe 'PUT /update' do
     it 'user情報の編集に成功' do
       user = FactoryBot.create(:user)
-      put "/v1/users/#{user.id}", params: {
-        user: { name: 'new_name', profile: 'new_profile' }
-      }
+      put "/v1/users/#{user.id}", params: { user: { name: 'new_name', profile: 'new_profile' } }
       json = JSON.parse(response.body)
-
       # リクエストが成功するか確認
       expect(response.status).to eq(200)
       # 値が変更できたか確認
       expect(json['name']).to eq('new_name')
       expect(json['name']).to_not eq('user1@aa.com')
-
+      # 値が変更できたか確認
       expect(json['profile']).to eq('new_profile')
       expect(json['profile']).to_not eq('よろしくお願いします！')
     end
 
     it 'user情報の編集に失敗' do
       user = FactoryBot.create(:user)
-      put "/v1/users/#{user.id}", params: {
-        user: { name: '' }
-      }
+      put "/v1/users/#{user.id}", params: { user: { name: '' } }
       json = JSON.parse(response.body)
-
       # リクエストが成功するか確認
       expect(response.status).to eq(200)
       # エラーが出るか確認
       expect(json['name']).to eq(['を入力してください'])
+    end
+  end
+
+  describe 'DELETE /destroy' do
+    it 'user情報の削除に成功' do
+      user = FactoryBot.create(:user)
+      # データが減るか確認
+      expect { delete "/v1/users/#{user.id}" }.to change(User, :count).by(-1)
+      # リクエストが成功するか確認
+      expect(response.status).to eq(200)
     end
   end
 end
