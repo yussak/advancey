3095e13691e8448161789f851acbe3f0aacf8eba
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-05-12 20:33:56 +0900

    [style]カルーセル調整

--- front/components/Carousel.vue
+++ front/components/Carousel.vue
@@ -14,7 +14,7 @@
         >
           <v-row class="fill-height" align="center" justify="center">
             <div class="text-center">
-              <p class="text-h3">Advanceyの使い方</p>
+              <p class="text-h4">Advanceyの使い方</p>
             </div>
           </v-row>
         </v-sheet>
@@ -23,12 +23,12 @@
         <v-sheet color="pink lighten-4" height="100%" style="padding: 0 30px">
           <v-row class="fill-height" align="center" justify="center">
             <div class="text-center">
-              <p class="text-h4">メモを追加しましょう</p>
-              <p class="text-h5">役立ったことなど何でもメモしてみましょう</p>
-              <p class="text-h5">
+              <p class="text-h5">メモを追加しましょう</p>
+              <p class="text-h6">役立ったことなど何でもメモしてみましょう</p>
+              <p class="text-h6">
                 やっていること・やりたいこと・やってよかったことに<br />分類できます
               </p>
-              <p class="text-h5">非公開にすると自分だけが閲覧できます</p>
+              <p class="text-h6">非公開にすると自分だけが閲覧できます</p>
             </div>
           </v-row>
         </v-sheet>
@@ -37,9 +37,9 @@
         <v-sheet color="blue lighten-4" height="100%" style="padding: 0 30px">
           <v-row class="fill-height" align="center" justify="center">
             <div class="text-center">
-              <p class="text-h4">質問をしてみましょう</p>
-              <p class="text-h5">分かる質問には回答してみましょう</p>
-              <p class="text-h5">解決した問題はクローズすることができます。</p>
+              <p class="text-h5">質問をしてみましょう</p>
+              <p class="text-h6">分かる質問には回答してみましょう</p>
+              <p class="text-h6">解決した問題はクローズすることができます。</p>
             </div>
           </v-row>
         </v-sheet>
@@ -48,11 +48,11 @@
         <v-sheet color="lime lighten-4" height="100%" style="padding: 0 30px">
           <v-row class="fill-height" align="center" justify="center">
             <div class="text-center">
-              <p class="text-h4">目標を立てましょう</p>
-              <p class="text-h5">
+              <p class="text-h5">目標を立てましょう</p>
+              <p class="text-h6">
                 達成したいこと・その理由・やること<br />を明確にし、継続・達成しましょう
               </p>
-              <p class="text-h5">カレンダーに毎日コメントできます</p>
+              <p class="text-h6">カレンダーに毎日コメントできます</p>
             </div>
           </v-row>
         </v-sheet>
@@ -61,10 +61,10 @@
         <v-sheet color="orange lighten-4" height="100%" style="padding: 0 30px">
           <v-row class="fill-height" align="center" justify="center">
             <div class="text-center">
-              <p class="text-h4">コミュニティに参加しましょう</p>
-              <p class="text-h5">テーマを決めて仲間と集まりましょう</p>
-              <p class="text-h5">リアルタイムチャットで盛り上がりましょう</p>
-              <p class="text-h5">出入りは自由です</p>
+              <p class="text-h5">コミュニティに参加しましょう</p>
+              <p class="text-h6">テーマを決めて仲間と集まりましょう</p>
+              <p class="text-h6">リアルタイムチャットで盛り上がりましょう</p>
+              <p class="text-h6">出入りは自由です</p>
             </div>
           </v-row>
         </v-sheet>
