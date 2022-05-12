27f892038bb808c44d1ecb4c4e608b247e2f79f8
yusuke sakuraba <yusuke.sakuraba0213@gmail.com>
2022-05-12 23:29:52 +0900

    [style]Post削除テスト追加

--- api/spec/requests/api/v1/posts_spec.rb
+++ api/spec/requests/api/v1/posts_spec.rb
@@ -1,17 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::Posts', type: :request do
-  describe 'POST /create' do
-    it 'post追加ページを表示' do
-      user = FactoryBot.create(:user)
-      post_params = { user_id: '1', content: 'asdf' }
-      # データが増えるか確認
-      expect { post '/v1/posts', params: { post: post_params } }.to change(Post, :count).by(+1)
-      # リクエストが成功するか確認
-      expect(response.status).to eq(200)
-    end
-  end
-
   describe 'GET /index' do
     it 'post一覧ページを表示' do
       post = FactoryBot.create(:post)
@@ -25,6 +14,27 @@ RSpec.describe 'Api::V1::Posts', type: :request do
 
       expect(json[1]['content']).to eq(post.content)
       expect(json[0]['content']).to eq(other_post.content)
+    end
+  end
+
+  describe 'POST /create' do
+    it 'post追加ページを表示' do
+      user = FactoryBot.create(:user)
+      post_params = { user_id: '1', content: 'asdf' }
+      # データが増えるか確認
+      expect { post '/v1/posts', params: { post: post_params } }.to change(Post, :count).by(+1)
+      # リクエストが成功するか確認
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe 'POST /delete' do
+    it 'post削除ページを表示' do
+      post = FactoryBot.create(:post)
+      # データが減るか確認
+      expect { delete "/v1/posts/#{post.id}" }.to change(Post, :count).by(-1)
+      # リクエストが成功するか確認
+      expect(response.status).to eq(200)
     end
   end
 end
